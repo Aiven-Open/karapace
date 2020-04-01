36cc0bdf3058d13b4d24f7c5e3970c89255012fc
Gabriel Tincu <gabi@aiven.io>
2020-04-01 14:40:28 +0200

    More thorough close procedure, all unit tests are passing - Replace time.sleep with asyncio.sleep in relevant places due to blocking nature of first - Add a small non blocking wait period to queue offset verification method - Add proper close methods for all relevant objects (still need to figure where to   hook the main Karapace close method) - Inserted awaitable futures in some objects so we can only move forward with   processing once initialization is properly in place - Karapace fixture is async so it can properly call the cleanup method - Update ssl context loading code from conn.py as it provides more error/options checking

--- karapace/config.py
+++ karapace/config.py
@@ -7,6 +7,7 @@ See LICENSE for details
 import socket
 import ssl
 
+
 def set_config_defaults(config):
     config.setdefault("advertised_hostname", socket.gethostname())
     config.setdefault("bootstrap_uri", "127.0.0.1:9092")
@@ -28,7 +29,27 @@ def set_config_defaults(config):
 
 
 def create_ssl_context(config):
-    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    context.load_cert_chain(certfile=config["ssl_certfile"], keyfile=config["ssl_keyfile"])
-    context.load_verify_locations(config["ssl_cafile"])
-    return context
+    # taken from conn.py, as it adds a lot more logic to the context configuration than the initial version
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)  # pylint: disable=no-member
+    ssl_context.options |= ssl.OP_NO_SSLv2  # pylint: disable=no-member
+    ssl_context.options |= ssl.OP_NO_SSLv3  # pylint: disable=no-member
+    ssl_context.verify_mode = ssl.CERT_OPTIONAL
+    if config['ssl_check_hostname']:
+        ssl_context.check_hostname = True
+    if config['ssl_cafile']:
+        ssl_context.load_verify_locations(config['ssl_cafile'])
+        ssl_context.verify_mode = ssl.CERT_REQUIRED
+    if config['ssl_certfile'] and config['ssl_keyfile']:
+        ssl_context.load_cert_chain(
+            certfile=config['ssl_certfile'],
+            keyfile=config['ssl_keyfile'],
+            password=config['ssl_password'])
+    if config['ssl_crlfile']:
+        if not hasattr(ssl, 'VERIFY_CRL_CHECK_LEAF'):
+            raise RuntimeError('This version of Python does not support ssl_crlfile!')
+        ssl_context.load_verify_locations(config['ssl_crlfile'])
+        # pylint: disable=no-member
+        ssl_context.verify_flags |= ssl.VERIFY_CRL_CHECK_LEAF
+    if config['ssl_ciphers']:
+        ssl_context.set_ciphers(config['ssl_ciphers'])
+    return ssl_context
--- tests/test_master_coordinator.py
+++ tests/test_master_coordinator.py
@@ -6,6 +6,7 @@ See LICENSE for details
 """
 from karapace.config import set_config_defaults
 from karapace.master_coordinator import MasterCoordinator
+
 import asyncio
 import logging
 
@@ -13,46 +14,43 @@ log = logging.getLogger(__name__)
 
 pytest_plugins = "aiohttp.pytest_plugin"
 
+
 class Timeout(Exception):
     pass
 
 
-async def init_admin(config):
-    mc = MasterCoordinator(config=config)
-    asyncio.create_task(mc.run())
+async def init_admin(config, loop):
+    mc = MasterCoordinator(config=config, loop=loop)
     return mc
 
 
 async def test_master_selection(kafka_server):
+    loop = asyncio.get_event_loop()
     config_aa = set_config_defaults({})
     config_aa["advertised_hostname"] = "127.0.0.1"
     config_aa["bootstrap_uri"] = "127.0.0.1:{}".format(kafka_server["kafka_port"])
     config_aa["client_id"] = "aa"
     config_aa["port"] = 1234
 
-    mc_aa = await init_admin(config_aa)
-
+    mc_aa = await init_admin(config_aa, loop)
     config_bb = set_config_defaults({})
     config_bb["advertised_hostname"] = "127.0.0.1"
     config_bb["bootstrap_uri"] = "127.0.0.1:{}".format(kafka_server["kafka_port"])
     config_bb["client_id"] = "bb"
     config_bb["port"] = 5678
-    mc_bb = await init_admin(config_bb)
+    mc_bb = await init_admin(config_bb, loop)
+    await asyncio.gather(mc_aa.running, mc_bb.running, loop=loop)
+    await asyncio.gather(mc_aa.sc.first_join, mc_bb.sc.first_join, loop=loop)
     while True:
-        if not (mc_aa.sc or mc_bb.sc):
-            log.info("waiting for %r or %r to be initialized", mc_aa.sc, mc_bb.sc)
-            await asyncio.sleep(1.0)
-            continue
         if not (mc_aa.sc.master or mc_bb.sc.master or mc_aa.sc.master_url or mc_bb.sc.master_url):
-            log.info("waiting for %r, %r, %r or %r to be initialized", mc_aa.sc.master, mc_bb.sc.master,
-                     mc_aa.sc.master_url, mc_bb.sc.master_url)
             await asyncio.sleep(1.0)
             continue
+        log.info("mc_aa.sc.master: %r, mc_bb.sc.master: %r, mc_aa.sc.master_url: %r, mc_bb.sc.master_url: %r",
+                 mc_aa.sc.master, mc_bb.sc.master, mc_aa.sc.master_url, mc_bb.sc.master_url)
         assert mc_aa.sc.master is True
         assert mc_bb.sc.master is False
         master_url = "http://{}:{}".format(config_aa["host"], config_aa["port"])
         assert mc_aa.sc.master_url == master_url
         assert mc_bb.sc.master_url == master_url
         break
-    mc_aa.close()
-    mc_bb.close()
+    await asyncio.gather(mc_aa.close(), mc_bb.close())
