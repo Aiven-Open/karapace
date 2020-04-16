60e33adbb06e6d1dbca9dc9310730ab2009e1387
Gabriel Tincu <gabi@aiven.io>
2020-04-16 15:09:30 +0200

    Custom content type support / parsing for kafka rest requests - Json / b64 encoding, more logic for content type parsing, some request validation logic

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -1,9 +1,21 @@
 from .utils import Client
 from kafka.admin import NewTopic
+from kafka.cluster import TopicPartition
 from kafka.errors import TopicAlreadyExistsError, UnknownTopicOrPartitionError
 from pytest import raises
 
 pytest_plugins = "aiohttp.pytest_plugin"
+HEADERS = {
+    "json": {
+        "Content-Type": "application/vnd.kafka.json.v2+json"
+    },
+    "base64": {
+        "Content-Type": "application/vnd.kafka.binary.v2+json"
+    },
+    "avro": {
+        "Content-Type": "application/vnd.kafka.avro.v2+json"
+    },
+}
 
 
 def test_admin_client(admin_client, producer):
@@ -15,10 +27,11 @@ def test_admin_client(admin_client, producer):
         pass
     topic_info = admin_client.cluster_metadata()
     retrieved_names = list(topic_info["topics"].keys())
-    assert set(retrieved_names) == set(topic_names), "Returned value %r differs from written one %r" % \
-                                                     (retrieved_names, topic_names)
+    assert set(topic_names).difference(set(retrieved_names)) == set(), \
+        "Returned value %r differs from written one %r" % (retrieved_names, topic_names)
     assert len(topic_info["brokers"]) == 1, "Only one broker during tests"
-    for v in topic_info["topics"].values():
+    for t in topic_names:
+        v = topic_info["topics"][t]
         assert len(v["partitions"]) == 1, "Should only have data for one partition"
         details = v["partitions"][0]
         assert len(details["replicas"]) == 1, "Should have only 1 replica"
@@ -57,15 +70,40 @@ async def test_local(kafka_rest, aiohttp_client, producer, admin_client):
     except TopicAlreadyExistsError:
         pass
     await check_brokers(c)
+    await check_malformed_requests(c)
     await check_topics(c, topic_names)
     await check_partitions(c, "topic_1", producer)
+
+
+async def test_internal(kafka_rest, admin_client):
+    try:
+        admin_client.create_topics([NewTopic(name="topic_2", num_partitions=1, replication_factor=1)])
+    except TopicAlreadyExistsError:
+        pass
+    kc, _ = kafka_rest()
+    for p in [0, None]:
+        result = kc.produce_message(topic="topic_2", key=b"key", value=b"value", partition=p)
+        assert "error" not in result, "Valid result should not contain 'error' key"
+        assert "offset" in result, "Valid result is missing 'offset' key"
+        assert "partition" in result, "Valid result is missing 'partition' key"
+        expected_part = TopicPartition("topic_2", 0)
+        actual_part = result["partition"]
+        assert actual_part == expected_part, "Missing partition publish should go to %r but went to %r" % \
+                                             (expected_part, actual_part)
+    result = kc.produce_message(topic="topic_2", key=b"key", value=b"value", partition=100)
+    assert "error" in result, "Invalid result missing 'error' key"
+    assert result["error"] == "Unrecognized partition"
+    assert "error_code" in result, "Invalid result missing 'error_code' key"
+    assert result["error_code"] == 1
+    assert kc.has_non_empty({"records": [{"key": {"foo": "bar"}}]}, "key") is True
+    assert kc.has_non_empty({"records": [{"value": {"foo": "bar"}}]}, "value") is True
 
 
 async def check_topics(c, topic_names):
     res = await c.get("/topics")
     assert res.status_code == 200, "Status code is not 200: %r" % res.status_code
     data = res.json()
-    assert set(topic_names) == set(data), "Retrieved topic names do not match: %r" % data
+    assert set(topic_names).difference(set(data)) == set(), "Retrieved topic names do not match: %r" % data
     res = await c.get("/topics/topic_0")
     assert res.status_code == 200, "Status code is not 200: %r" % res.status_code
     data = res.json()
@@ -81,6 +119,36 @@ async def check_topics(c, topic_names):
     res = await c.get("/topics/foo")
     assert res.status_code == 404, "Topic should not exist"
     assert res.json()["error_code"] == 40401, "Error code does not match"
+    # publish
+    # Proper Json / binary
+    res = await c.post("/topics/topic_0", json={"records": [{"value": {"foo": "bar"}}]}, headers=HEADERS["json"])
+    json = res.json()
+    assert res.status == 200
+    assert "offsets" in json
+
+
+async def check_malformed_requests(c):
+    # Malformed schema
+    res = await c.post("/topics/topic_0", json={"foo": "bar"}, headers=HEADERS["json"])
+    json = res.json()
+    assert res.status == 500
+    assert json["message"] == "Invalid request format"
+    res = await c.post("/topics/topic_0", json={"records": [{"value": {"foo": "bar"}}]}, headers=HEADERS["avro"])
+    json = res.json()
+    assert res.status == 422
+    assert json["error_code"] == 42202
+    res = await c.post("/topics/topic_0", json={"records": [{"key": {"foo": "bar"}}]}, headers=HEADERS["avro"])
+    json = res.json()
+    assert res.status == 422
+    assert json["error_code"] == 42201
+    res = await c.post("/topics/topic_0", json={"records": [{"value": "not base64"}]}, headers=HEADERS["base64"])
+    json = res.json()
+    assert res.status == 422
+    assert json["error_code"] == 42205
+    res = await c.post("/topics/topic_0", json={"records": [{"value": "not json"}]}, headers=HEADERS["json"])
+    json = res.json()
+    assert res.status == 422
+    assert json["error_code"] == 42205
 
 
 async def check_brokers(c):
