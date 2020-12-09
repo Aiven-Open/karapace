8e3948318065fd00a0ad433c6bd1239d1d3f02d6
Augusto F. Hack <hack.augusto@gmail.com>
2020-12-09 17:28:02 +0100

    Added typing info to conftest.py

--- tests/test_master_coordinator.py
+++ tests/test_master_coordinator.py
@@ -6,6 +6,8 @@ See LICENSE for details
 """
 from karapace.config import set_config_defaults
 from karapace.master_coordinator import MasterCoordinator
+from tests.utils import KafkaConfig, REGISTRY_URI, REST_URI
+from typing import Optional
 
 import asyncio
 import json
@@ -26,17 +28,19 @@ def init_admin(config):
 
 
 @pytest.mark.parametrize("strategy", ["lowest", "highest"])
-def test_master_selection(kafka_server, strategy):
+def test_master_selection(kafka_server: Optional[KafkaConfig], strategy: str) -> None:
+    assert kafka_server, f"test_master_selection can not be used if the env variable `{REGISTRY_URI}` or `{REST_URI}` is set"
+
     config_aa = set_config_defaults({})
     config_aa["advertised_hostname"] = "127.0.0.1"
-    config_aa["bootstrap_uri"] = "127.0.0.1:{}".format(kafka_server["kafka_port"])
+    config_aa["bootstrap_uri"] = "127.0.0.1:{}".format(kafka_server.kafka_port)
     config_aa["client_id"] = "aa"
     config_aa["port"] = 1234
     config_aa["master_election_strategy"] = strategy
     mc_aa = init_admin(config_aa)
     config_bb = set_config_defaults({})
     config_bb["advertised_hostname"] = "127.0.0.1"
-    config_bb["bootstrap_uri"] = "127.0.0.1:{}".format(kafka_server["kafka_port"])
+    config_bb["bootstrap_uri"] = "127.0.0.1:{}".format(kafka_server.kafka_port)
     config_bb["client_id"] = "bb"
     config_bb["port"] = 5678
     config_bb["master_election_strategy"] = strategy
