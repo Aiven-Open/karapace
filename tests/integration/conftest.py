31a035b2030e9e104b95330fd9908fbbe19b4584
Augusto F. Hack <hack.augusto@gmail.com>
2021-02-12 21:44:13 +0100

    tests: kill server if the port is not available within timeout

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -293,9 +293,9 @@ def fixture_registry_async_pair(session_tmpdir: TempDirCreator, kafka_server: Op
     )
     master_process = subprocess.Popen(["python", "-m", "karapace.karapace_all", master_config_path])
     slave_process = subprocess.Popen(["python", "-m", "karapace.karapace_all", slave_config_path])
-    wait_for_port(master_port)
-    wait_for_port(slave_port)
     try:
+        wait_for_port(master_port)
+        wait_for_port(slave_port)
         yield f"http://127.0.0.1:{master_port}", f"http://127.0.0.1:{slave_port}"
     finally:
         master_process.kill()
