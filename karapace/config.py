50c97b22f72383c33b187541d567d58323c9124a
Augusto F. Hack <hack.augusto@gmail.com>
2021-02-16 12:26:48 +0100

    config: extended typing for it

--- karapace/config.py
+++ karapace/config.py
@@ -12,7 +12,8 @@ import os
 import socket
 import ssl
 
-DEFAULTS = {
+CONFIG_TYPE = Dict[str, Union[None, str, int, bool]]
+DEFAULTS: CONFIG_TYPE = {
     "advertised_hostname": socket.gethostname(),
     "bootstrap_uri": "127.0.0.1:9092",
     "client_id": "sr-1",
@@ -65,7 +66,7 @@ def parse_env_value(value: str) -> Union[str, int, bool]:
     return value
 
 
-def set_config_defaults(config: Dict[str, Union[str, int, bool]]) -> Dict[str, Union[str, int, bool]]:
+def set_config_defaults(config: CONFIG_TYPE) -> CONFIG_TYPE:
     for k, v in DEFAULTS.items():
         if k.startswith("karapace"):
             env_name = k.upper()
@@ -76,8 +77,12 @@ def set_config_defaults(config: Dict[str, Union[str, int, bool]]) -> Dict[str, U
             print(f"Populating config value {k} from env var {env_name} with {val} instead of config file")
             config[k] = parse_env_value(os.environ[env_name])
         config.setdefault(k, v)
+
     strat = config["master_election_strategy"]
-    assert strat.lower() in {"highest", "lowest"}, f"Invalid master election strategy: {strat}"
+    msg = 'master_election_strategy must be "highest" or "lowest"'
+    assert isinstance(strat, str), msg
+    assert strat.lower() in {"highest", "lowest"}, msg
+
     return config
 
 
@@ -91,10 +96,10 @@ def read_config(config_handler: IO) -> Dict[str, Union[str, int, bool]]:
         config = set_config_defaults(config)
         return config
     except Exception as ex:
-        raise InvalidConfiguration(ex)
+        raise InvalidConfiguration(ex) from None
 
 
-def create_ssl_context(config: Dict[str, Union[str, int, bool]]) -> ssl.SSLContext:
+def create_ssl_context(config: CONFIG_TYPE) -> ssl.SSLContext:
     # taken from conn.py, as it adds a lot more logic to the context configuration than the initial version
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)  # pylint: disable=no-member
     ssl_context.options |= ssl.OP_NO_SSLv2  # pylint: disable=no-member
@@ -102,21 +107,37 @@ def create_ssl_context(config: Dict[str, Union[str, int, bool]]) -> ssl.SSLConte
     ssl_context.options |= ssl.OP_NO_TLSv1  # pylint: disable=no-member
     ssl_context.options |= ssl.OP_NO_TLSv1_1  # pylint: disable=no-member
     ssl_context.verify_mode = ssl.CERT_OPTIONAL
+
     if config.get('ssl_check_hostname'):
         ssl_context.check_hostname = True
-    if config['ssl_cafile']:
+
+    if isinstance(config['ssl_cafile'], str):
         ssl_context.load_verify_locations(config['ssl_cafile'])
         ssl_context.verify_mode = ssl.CERT_REQUIRED
-    if config['ssl_certfile'] and config['ssl_keyfile']:
-        ssl_context.load_cert_chain(
-            certfile=config['ssl_certfile'], keyfile=config['ssl_keyfile'], password=config.get('ssl_password')
-        )
+
+    if isinstance(config['ssl_certfile'], str) and isinstance(config['ssl_keyfile'], str):
+        password = config.get('ssl_password')
+        if not isinstance(password, str):
+            raise ValueError("Invalid password type, it should be a string")
+
+        ssl_context.load_cert_chain(certfile=config['ssl_certfile'], keyfile=config['ssl_keyfile'], password=password)
+
     if config.get('ssl_crlfile'):
         if not hasattr(ssl, 'VERIFY_CRL_CHECK_LEAF'):
             raise RuntimeError('This version of Python does not support ssl_crlfile!')
-        ssl_context.load_verify_locations(config['ssl_crlfile'])
-        # pylint: disable=no-member
-        ssl_context.verify_flags |= ssl.VERIFY_CRL_CHECK_LEAF
+
+        ssl_crlfile = config['ssl_crlfile']
+        if not isinstance(ssl_crlfile, str):
+            raise ValueError("Invalid ssl_crlfile type, it should be a string")
+
+        ssl_context.load_verify_locations(ssl_crlfile)
+        ssl_context.verify_flags |= ssl.VERIFY_CRL_CHECK_LEAF  # pylint: disable=no-member
+
     if config.get('ssl_ciphers'):
-        ssl_context.set_ciphers(config['ssl_ciphers'])
+        ssl_ciphers = config['ssl_ciphers']
+        if not isinstance(ssl_ciphers, str):
+            raise ValueError("Invalid ssl_ciphers type, it should be a string")
+
+        ssl_context.set_ciphers(ssl_ciphers)
+
     return ssl_context
