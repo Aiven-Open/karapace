54825ca51b0ccffc48209f418e63f123eb3d5fdc
Augusto F. Hack <hack.augusto@gmail.com>
2021-02-16 12:27:27 +0100

    pylint: super should be called without arguments

--- karapace/master_coordinator.py
+++ karapace/master_coordinator.py
@@ -96,16 +96,15 @@ class SchemaCoordinator(BaseCoordinator):
         else:
             self.master_url = master_url
             self.master = False
-        return super(SchemaCoordinator, self)._on_join_complete(generation, member_id, protocol, member_assignment_bytes)
+        return super()._on_join_complete(generation, member_id, protocol, member_assignment_bytes)
 
     def _on_join_follower(self):
         self.log.info("We are a follower, not a master")
-        return super(SchemaCoordinator, self)._on_join_follower()
+        return super()._on_join_follower()
 
 
 class MasterCoordinator(Thread):
     """Handles schema topic creation and master election"""
-
     def __init__(self, config):
         Thread.__init__(self)
         self.config = config
