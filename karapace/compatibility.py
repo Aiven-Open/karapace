111c64405ac0f4a9aff8ad195df6dd5a86f9a26a
Augusto F. Hack <hack.augusto@gmail.com>
2021-02-16 15:35:39 +0100

    compatibility: removed duplicated logic
    Made the avro compatibility check more explicit. The code was relying on
    a "fall-through" for the FULL checks to work, this commit changes the
    code so that FULL calls explicitly both the forward and backwards
    direction.

--- karapace/compatibility.py
+++ karapace/compatibility.py
@@ -18,7 +18,16 @@ class IncompatibleSchema(Exception):
     pass
 
 
-def check_compatibility(source: TypedSchema, target: TypedSchema, compatibility: str) -> bool:
+def check_avro_compatibility(reader_schema, writer_schema) -> None:
+    result = AvroChecker().get_compatibility(reader=reader_schema, writer=writer_schema)
+    if (
+        result.compatibility is SchemaCompatibilityType.incompatible
+        and [SchemaIncompatibilityType.missing_enum_symbols] != result.incompatibilities
+    ):
+        raise IncompatibleSchema(str(result.compatibility))
+
+
+def check_compatibility(source: TypedSchema, target: TypedSchema, compatibility: str) -> None:
     if source.schema_type is not target.schema_type:
         raise IncompatibleSchema(f"Comparing different schema types: {source.schema_type} with {target.schema_type}")
 
@@ -26,22 +35,15 @@ def check_compatibility(source: TypedSchema, target: TypedSchema, compatibility:
     checking_for = compatibility.split("_")[0]
     if checking_for == "NONE":
         LOG.info("Compatibility level set to NONE, no schema compatibility checks performed")
-        return True
+        return
 
     if source.schema_type is SchemaType.AVRO:
-        if checking_for in {"BACKWARD", "FULL"}:
-            writer, reader = source.schema, target.schema
-            result = AvroChecker().get_compatibility(reader=reader, writer=writer)
-            if (
-                result.compatibility is SchemaCompatibilityType.incompatible
-                and [SchemaIncompatibilityType.missing_enum_symbols] != result.incompatibilities
-            ):
-                raise IncompatibleSchema(str(result.compatibility))
-        if checking_for in {"FORWARD", "FULL"}:
-            writer, reader = target.schema, source.schema
-            result = AvroChecker().get_compatibility(reader=reader, writer=writer)
-            if (
-                result.compatibility is SchemaCompatibilityType.incompatible
-                and [SchemaIncompatibilityType.missing_enum_symbols] != result.incompatibilities
-            ):
-                raise IncompatibleSchema(str(result.compatibility))
+        if checking_for == "BACKWARD":
+            check_avro_compatibility(writer_schema=source.schema, reader_schema=target.schema)
+        elif checking_for == "FORWARD":
+            check_avro_compatibility(writer_schema=target.schema, reader_schema=source.schema)
+        elif checking_for == "FULL":
+            check_avro_compatibility(writer_schema=source.schema, reader_schema=target.schema)
+            check_avro_compatibility(writer_schema=target.schema, reader_schema=source.schema)
+
+    LOG.info("Unknow schema_type %r", source.schema_type)
