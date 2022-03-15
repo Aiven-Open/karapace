9266939c25942daa41f91d6069a52778b88b5754
Augusto F. Hack <hacka@aiven.io>
2022-03-15 19:28:54 +0100

    tests: network utils are used by integration tests only

--- tests/integration/utils/kafka_server.py
+++ tests/integration/utils/kafka_server.py
@@ -8,8 +8,9 @@ from karapace.kafka_rest_apis import KafkaRestAdminClient
 from pathlib import Path
 from subprocess import Popen
 from tests.integration.utils.config import KafkaConfig, KafkaDescription, ZKConfig
+from tests.integration.utils.network import get_random_port, KAFKA_PORT_RANGE
 from tests.integration.utils.process import get_java_process_configuration
-from tests.utils import Expiration, get_random_port, KAFKA_PORT_RANGE, write_ini
+from tests.utils import Expiration, write_ini
 from typing import Dict, List, Tuple
 
 import logging
