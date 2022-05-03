65a0f5fd6efbbe67300f97ce06411afcfb9157e5
YuWang24 <56781690+YuWang24@users.noreply.github.com>
2022-05-03 20:10:07 +1000

    Add new configs for advertised_port and advertised_protocol
    REST API for consumer returns `base_uri`, which in case of using reverse proxy in front of Karapace needs to be configured for public URI.  This adds new configs for `advertised_port` and `advertised_protocol` in addition to existing `advertised_hostname` for ability to fully customize this.
    
    Co-authored-by: Yu Wang <yu.wang@instaclustr.com>

--- karapace/kafka_rest_apis/consumer_manager.py
+++ karapace/kafka_rest_apis/consumer_manager.py
@@ -34,7 +34,15 @@ def new_name() -> str:
 class ConsumerManager:
     def __init__(self, config: dict) -> None:
         self.config = config
-        self.hostname = f"http://{self.config['advertised_hostname']}:{self.config['port']}"
+        if self.config["advertised_port"] is None:
+            self.hostname = (
+                f"{self.config['advertised_protocol']}://{self.config['advertised_hostname']}:{self.config['port']}"
+            )
+        else:
+            self.hostname = (
+                f"{self.config['advertised_protocol']}://"
+                f"{self.config['advertised_hostname']}:{self.config['advertised_port']}"
+            )
         self.deserializer = SchemaRegistryDeserializer(config=config)
         self.consumers = {}
         self.consumer_locks = defaultdict(Lock)
