cfae4ca9c3b46cf6100fc67afc7c18c4483eb71e
Tommi Vainikainen <tvainika@aiven.io>
2022-10-05 10:41:02 +0300

    Fix Internal Server Error deleting unknown subject
    Catch exception if subject know found correctly, and add integration
    tests for this behavior.

--- karapace/schema_registry_apis.py
+++ karapace/schema_registry_apis.py
@@ -613,6 +613,15 @@ class KarapaceSchemaRegistryController(KarapaceBase):
             try:
                 version_list = await self.schema_registry.subject_delete_local(subject=subject, permanent=permanent)
                 self.r(version_list, content_type, status=HTTPStatus.OK)
+            except (SubjectNotFoundException, SchemasNotFoundException):
+                self.r(
+                    body={
+                        "error_code": SchemaErrorCodes.SUBJECT_NOT_FOUND.value,
+                        "message": SchemaErrorMessages.SUBJECT_NOT_FOUND_FMT.value.format(subject=subject),
+                    },
+                    content_type=content_type,
+                    status=HTTPStatus.NOT_FOUND,
+                )
             except SubjectNotSoftDeletedException:
                 self.r(
                     body={
@@ -681,7 +690,7 @@ class KarapaceSchemaRegistryController(KarapaceBase):
             try:
                 resolved_version = await self.schema_registry.subject_version_delete_local(subject, version, permanent)
                 self.r(str(resolved_version), content_type, status=HTTPStatus.OK)
-            except SubjectNotFoundException:
+            except (SubjectNotFoundException, SchemasNotFoundException):
                 self.r(
                     body={
                         "error_code": SchemaErrorCodes.SUBJECT_NOT_FOUND.value,
