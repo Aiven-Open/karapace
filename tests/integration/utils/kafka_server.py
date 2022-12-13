91a14d8c896cd1a9a0ad2433305a3b8c5dbead29
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-13 12:33:52 +0000

    Adding tarfile member sanitization to extractall()

--- tests/integration/utils/kafka_server.py
+++ tests/integration/utils/kafka_server.py
@@ -76,7 +76,26 @@ def maybe_download_kafka(kafka_description: KafkaDescription) -> None:
 
         download = requests.get(kafka_description.download_url, stream=True)
         with tarfile.open(mode="r:gz", fileobj=download.raw) as file:
-            file.extractall(str(kafka_description.install_dir.parent))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(file, str(kafka_description.install_dir.parent))
 
 
 def kafka_java_args(
