81110515d6e3f5c3b0a32e988dab1470517b6d81
Anton Agestam <anton.agestam@aiven.io>
2023-01-27 11:10:18 +0100

    chore: Simplify pre-commit config
    Change execution order of pre-commit hooks. It's a good idea to allow
    isort to run before black, to let black be authoritative on formatting.
    The very fast builtin pre-commit checks are moved to top to always
    execute first.
    
    Some superfluous configuration options are removed, specifically there's
    no need to point out that Python tools should run on Python files, this
    is already configured in the hooks themselves.
    
    By default, flake8 uses the .flake8 config file.
    
    Black line-length config is moved from a hard-coded CLI argument to
    pyproject.toml which is picked up by default, and target-version=37 is
    added there explicitly.
    
    #### Misc.:
    
    - Advertise support for Python 3.11 with a setup.py classifier.
    - Fix a typo in README.

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Database :: Database Engines/Servers",
         "Topic :: Software Development :: Libraries",
     ],
