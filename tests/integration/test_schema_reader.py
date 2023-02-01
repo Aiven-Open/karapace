TrellixVulnTeam/karapace_64VG

refactor: add dictionary database storage abstraction
The storage abstraction was in the schema reader.
This refactoring separates the storage to own module.

tests/integration/test_schema_reader.py
@@ -3,13 +3,14 @@ from dataclasses import dataclass
 from kafka import KafkaAdminClient, KafkaProducer
 from karapace.config import set_config_defaults
 from karapace.constants import DEFAULT_SCHEMA_TOPIC
+from karapace.dict_database import SchemaRegistryDictDatabase
 from karapace.key_format import KeyFormatter, KeyMode
 from karapace.master_coordinator import MasterCoordinator
 from karapace.schema_reader import KafkaSchemaReader
 from karapace.utils import json_encode
 from tests.base_testcase import BaseTestCase
 from tests.integration.utils.kafka_server import KafkaServers
-from tests.schemas.json_schemas import TRUE_SCHEMA
+from tests.schemas.json_schemas import FALSE_SCHEMA, TRUE_SCHEMA
 from tests.utils import create_group_name_factory, create_subject_name_factory, new_random_name, new_topic
 from typing import List, Tuple
 
@@ -65,7 +66,13 @@ def test_regression_soft_delete_schemas_should_be_registered(
     )
     master_coordinator = MasterCoordinator(config=config)
     master_coordinator.start()
-    schema_reader = KafkaSchemaReader(config=config, key_formatter=KeyFormatter(), master_coordinator=master_coordinator)
+    database = SchemaRegistryDictDatabase()
+    schema_reader = KafkaSchemaReader(
+        config=config,
+        key_formatter=KeyFormatter(),
+        master_coordinator=master_coordinator,
+        database=database,
+    )
     schema_reader.start()
 
     with closing(master_coordinator), closing(schema_reader):
@@ -94,7 +101,7 @@ def test_regression_soft_delete_schemas_should_be_registered(
 
         schema_reader.offset_watcher.wait_for_offset(msg.offset, timeout=5)
 
-        schemas = schema_reader.get_schemas(subject=subject, include_deleted=True)
+        schemas = database.find_subject_schemas(subject=subject, include_deleted=True)
         assert len(schemas) == 1, "Deleted schemas must have been registered"
 
         # Produce a soft deleted schema, this is the regression test
@@ -110,7 +117,7 @@ def test_regression_soft_delete_schemas_should_be_registered(
             "id": test_global_schema_id,
             "subject": subject,
             "version": 2,
-            "schema": json_encode(TRUE_SCHEMA.schema),
+            "schema": json_encode(FALSE_SCHEMA.schema),
         }
         future = producer.send(
             topic_name,
@@ -120,9 +127,9 @@ def test_regression_soft_delete_schemas_should_be_registered(
         msg = future.get()
 
         assert schema_reader.offset_watcher.wait_for_offset(msg.offset, timeout=5) is True
-        assert schema_reader.global_schema_id == test_global_schema_id
+        assert database.global_schema_id == test_global_schema_id
 
-        schemas = schema_reader.get_schemas(subject=subject, include_deleted=True)
+        schemas = database.find_subject_schemas(subject=subject, include_deleted=True)
         assert len(schemas) == 2, "Deleted schemas must have been registered"
 
 
@@ -143,7 +150,13 @@ def test_regression_config_for_inexisting_object_should_not_throw(
     )
     master_coordinator = MasterCoordinator(config=config)
     master_coordinator.start()
-    schema_reader = KafkaSchemaReader(config=config, key_formatter=KeyFormatter(), master_coordinator=master_coordinator)
+    database = SchemaRegistryDictDatabase()
+    schema_reader = KafkaSchemaReader(
+        config=config,
+        key_formatter=KeyFormatter(),
+        master_coordinator=master_coordinator,
+        database=database,
+    )
     schema_reader.start()
 
     with closing(master_coordinator), closing(schema_reader):
@@ -165,7 +178,7 @@ def test_regression_config_for_inexisting_object_should_not_throw(
         msg = future.get()
 
         assert schema_reader.offset_watcher.wait_for_offset(msg.offset, timeout=5) is True
-        assert subject in schema_reader.subjects, "The above message should be handled gracefully"
+        assert database.find_subject(subject=subject) is not None, "The above message should be handled gracefully"
 
 
 @dataclass
@@ -236,7 +249,13 @@ def test_key_format_detection(
     master_coordinator = MasterCoordinator(config=config)
     master_coordinator.start()
     key_formatter = KeyFormatter()
-    schema_reader = KafkaSchemaReader(config=config, key_formatter=key_formatter, master_coordinator=master_coordinator)
+    database = SchemaRegistryDictDatabase()
+    schema_reader = KafkaSchemaReader(
+        config=config,
+        key_formatter=key_formatter,
+        master_coordinator=master_coordinator,
+        database=database,
+    )
     schema_reader.start()
 
     with closing(master_coordinator), closing(schema_reader):

tests/unit/test_schema_reader.py
@@ -7,6 +7,7 @@ See LICENSE for details
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
 from karapace.config import DEFAULTS
+from karapace.dict_database import SchemaRegistryDictDatabase
 from karapace.schema_reader import KafkaSchemaReader, OFFSET_EMPTY, OFFSET_UNINITIALIZED, OffsetsWatcher
 from tests.base_testcase import BaseTestCase
 from unittest.mock import Mock
@@ -138,7 +139,12 @@ def test_readiness_check(testcase: ReadinessTestCase) -> None:
     # Return dict {partition: offsets}, end offset is the next upcoming record offset
     consumer_mock.end_offsets.return_value = {0: testcase.end_offset}
 
-    schema_reader = KafkaSchemaReader(config=DEFAULTS, key_formatter=key_formatter_mock, master_coordinator=None)
+    schema_reader = KafkaSchemaReader(
+        config=DEFAULTS,
+        key_formatter=key_formatter_mock,
+        master_coordinator=None,
+        database=SchemaRegistryDictDatabase(),
+    )
     schema_reader.consumer = consumer_mock
     schema_reader.offset = testcase.cur_offset
 

Aiven-Open/karapace

tests/integration/test_schema_reader.py
@@ -7,13 +7,14 @@ from dataclasses import dataclass
 from kafka import KafkaAdminClient, KafkaProducer
 from karapace.config import set_config_defaults
 from karapace.constants import DEFAULT_SCHEMA_TOPIC
+from karapace.in_memory_database import InMemoryDatabase
 from karapace.key_format import KeyFormatter, KeyMode
 from karapace.master_coordinator import MasterCoordinator
 from karapace.schema_reader import KafkaSchemaReader
 from karapace.utils import json_encode
 from tests.base_testcase import BaseTestCase
 from tests.integration.utils.kafka_server import KafkaServers
-from tests.schemas.json_schemas import TRUE_SCHEMA
+from tests.schemas.json_schemas import FALSE_SCHEMA, TRUE_SCHEMA
 from tests.utils import create_group_name_factory, create_subject_name_factory, new_random_name, new_topic
 from typing import List, Tuple
 
@@ -69,7 +70,13 @@ def test_regression_soft_delete_schemas_should_be_registered(
     )
     master_coordinator = MasterCoordinator(config=config)
     master_coordinator.start()
-    schema_reader = KafkaSchemaReader(config=config, key_formatter=KeyFormatter(), master_coordinator=master_coordinator)
+    database = InMemoryDatabase()
+    schema_reader = KafkaSchemaReader(
+        config=config,
+        key_formatter=KeyFormatter(),
+        master_coordinator=master_coordinator,
+        database=database,
+    )
     schema_reader.start()
 
     with closing(master_coordinator), closing(schema_reader):
@@ -98,7 +105,7 @@ def test_regression_soft_delete_schemas_should_be_registered(
 
         schema_reader.offset_watcher.wait_for_offset(msg.offset, timeout=5)
 
-        schemas = schema_reader.get_schemas(subject=subject, include_deleted=True)
+        schemas = database.find_subject_schemas(subject=subject, include_deleted=True)
         assert len(schemas) == 1, "Deleted schemas must have been registered"
 
         # Produce a soft deleted schema, this is the regression test
@@ -114,7 +121,7 @@ def test_regression_soft_delete_schemas_should_be_registered(
             "id": test_global_schema_id,
             "subject": subject,
             "version": 2,
-            "schema": json_encode(TRUE_SCHEMA.schema),
+            "schema": json_encode(FALSE_SCHEMA.schema),
         }
         future = producer.send(
             topic_name,
@@ -124,9 +131,9 @@ def test_regression_soft_delete_schemas_should_be_registered(
         msg = future.get()
 
         assert schema_reader.offset_watcher.wait_for_offset(msg.offset, timeout=5) is True
-        assert schema_reader.global_schema_id == test_global_schema_id
+        assert database.global_schema_id == test_global_schema_id
 
-        schemas = schema_reader.get_schemas(subject=subject, include_deleted=True)
+        schemas = database.find_subject_schemas(subject=subject, include_deleted=True)
         assert len(schemas) == 2, "Deleted schemas must have been registered"
 
 
@@ -147,7 +154,13 @@ def test_regression_config_for_inexisting_object_should_not_throw(
     )
     master_coordinator = MasterCoordinator(config=config)
     master_coordinator.start()
-    schema_reader = KafkaSchemaReader(config=config, key_formatter=KeyFormatter(), master_coordinator=master_coordinator)
+    database = InMemoryDatabase()
+    schema_reader = KafkaSchemaReader(
+        config=config,
+        key_formatter=KeyFormatter(),
+        master_coordinator=master_coordinator,
+        database=database,
+    )
     schema_reader.start()
 
     with closing(master_coordinator), closing(schema_reader):
@@ -169,7 +182,7 @@ def test_regression_config_for_inexisting_object_should_not_throw(
         msg = future.get()
 
         assert schema_reader.offset_watcher.wait_for_offset(msg.offset, timeout=5) is True
-        assert subject in schema_reader.subjects, "The above message should be handled gracefully"
+        assert database.find_subject(subject=subject) is not None, "The above message should be handled gracefully"
 
 
 @dataclass
@@ -240,7 +253,13 @@ def test_key_format_detection(
     master_coordinator = MasterCoordinator(config=config)
     master_coordinator.start()
     key_formatter = KeyFormatter()
-    schema_reader = KafkaSchemaReader(config=config, key_formatter=key_formatter, master_coordinator=master_coordinator)
+    database = InMemoryDatabase()
+    schema_reader = KafkaSchemaReader(
+        config=config,
+        key_formatter=key_formatter,
+        master_coordinator=master_coordinator,
+        database=database,
+    )
     schema_reader.start()
 
     with closing(master_coordinator), closing(schema_reader):

tests/unit/test_schema_reader.py
@@ -8,6 +8,7 @@ See LICENSE for details
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
 from karapace.config import DEFAULTS
+from karapace.in_memory_database import InMemoryDatabase
 from karapace.schema_reader import KafkaSchemaReader, OFFSET_EMPTY, OFFSET_UNINITIALIZED, OffsetsWatcher
 from tests.base_testcase import BaseTestCase
 from unittest.mock import Mock
@@ -139,7 +140,12 @@ def test_readiness_check(testcase: ReadinessTestCase) -> None:
     # Return dict {partition: offsets}, end offset is the next upcoming record offset
     consumer_mock.end_offsets.return_value = {0: testcase.end_offset}
 
-    schema_reader = KafkaSchemaReader(config=DEFAULTS, key_formatter=key_formatter_mock, master_coordinator=None)
+    schema_reader = KafkaSchemaReader(
+        config=DEFAULTS,
+        key_formatter=key_formatter_mock,
+        master_coordinator=None,
+        database=InMemoryDatabase(),
+    )
     schema_reader.consumer = consumer_mock
     schema_reader.offset = testcase.cur_offset
 