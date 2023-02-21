7af015cf1ea983ac257914879448fb5b48776f15
Richard Fussenegger <richard.fussenegger@aiven.io>
2023-02-21 11:25:45 +0100

    feat: Add `shellcheck` and `shfmt` Pre-Commit Hooks
    This adds `shellcheck` and `shfmt` pre-commit hooks. I fixed the most pressing
    issues that were found, but did not go through the existing scripts otherwise.
    We are going to need more shell scripts to automate other things, this is why it
    is a good idea to add these tools to our verification chain.
    
    ~This also changes the GHA lint workflow to use the official pre-commit action,
    which also makes use of caching, and thus should be faster.~ The pre-commit
    action uses the `--color=always` option, and that option results in the action
    hanging forever if paired with local hooks, which we have.

--- performance-test/run-locust-test.sh
+++ performance-test/run-locust-test.sh
@@ -1,5 +1,5 @@
-#!/bin/env bash
-set -eo pipefail
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
 # The REST proxy address params
 BASE_URL=${BASE_URL:-http://localhost:8082}
@@ -10,14 +10,14 @@ CONCURRENCY=${CONCURRENCY:-50}
 LOCUST_FILE=${LOCUST_FILE:-"rest-proxy-post-topic-test.py"}
 
 GUI_PARAMS="--headless"
-if [ ! -z $LOCUST_GUI ]; then
+if [[ -n $LOCUST_GUI ]]; then
     GUI_PARAMS="--autostart"
 fi
 
 TOPIC="${TOPIC}" locust "${GUI_PARAMS}" \
-     --run-time "${DURATION}" \
-     --users "${CONCURRENCY}" \
-     -H "${BASE_URL}" \
-     --spawn-rate 0.50 \
-     --stop-timeout 5 \
-     --locustfile "${LOCUST_FILE}"
+    --run-time "${DURATION}" \
+    --users "${CONCURRENCY}" \
+    -H "${BASE_URL}" \
+    --spawn-rate 0.50 \
+    --stop-timeout 5 \
+    --locustfile "${LOCUST_FILE}"
