ca65ddde04b1dd49cf4324ede66e2f5d827d3c51
Elia Migliore <elia.migliore@aiven.io>
2023-06-23 14:40:36 +0200

    Fixed inconsistent schema during message produce, from now on it is possible to produce a message only if the schema sent with the record is registered to the topic.

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -15,7 +15,7 @@ async def test_remote_client(registry_async_client: Client) -> None:
     subject = new_random_name("subject")
     sc_id = await reg_cli.post_new_schema(subject, schema_avro)
     assert sc_id >= 0
-    stored_schema = await reg_cli.get_schema_for_id(sc_id)
+    stored_schema, _ = await reg_cli.get_schema_for_id(sc_id)
     assert stored_schema == schema_avro, f"stored schema {stored_schema.to_dict()} is not {schema_avro.to_dict()}"
     stored_id, stored_schema = await reg_cli.get_latest_schema(subject)
     assert stored_id == sc_id
@@ -29,7 +29,7 @@ async def test_remote_client_tls(registry_async_client_tls: Client) -> None:
     subject = new_random_name("subject")
     sc_id = await reg_cli.post_new_schema(subject, schema_avro)
     assert sc_id >= 0
-    stored_schema = await reg_cli.get_schema_for_id(sc_id)
+    stored_schema, _ = await reg_cli.get_schema_for_id(sc_id)
     assert stored_schema == schema_avro, f"stored schema {stored_schema.to_dict()} is not {schema_avro.to_dict()}"
     stored_id, stored_schema = await reg_cli.get_latest_schema(subject)
     assert stored_id == sc_id
--- tests/integration/test_client_protobuf.py
+++ tests/integration/test_client_protobuf.py
@@ -16,7 +16,7 @@ async def test_remote_client_protobuf(registry_async_client):
     subject = new_random_name("subject")
     sc_id = await reg_cli.post_new_schema(subject, schema_protobuf, None)
     assert sc_id >= 0
-    stored_schema = await reg_cli.get_schema_for_id(sc_id)
+    stored_schema, _ = await reg_cli.get_schema_for_id(sc_id)
     assert stored_schema == schema_protobuf, f"stored schema {stored_schema} is not {schema_protobuf}"
     stored_id, stored_schema = await reg_cli.get_latest_schema(subject)
     assert stored_id == sc_id
@@ -31,7 +31,7 @@ async def test_remote_client_protobuf2(registry_async_client):
     subject = new_random_name("subject")
     sc_id = await reg_cli.post_new_schema(subject, schema_protobuf, None)
     assert sc_id >= 0
-    stored_schema = await reg_cli.get_schema_for_id(sc_id)
+    stored_schema, _ = await reg_cli.get_schema_for_id(sc_id)
     assert stored_schema == schema_protobuf, f"stored schema {stored_schema} is not {schema_protobuf}"
     stored_id, stored_schema = await reg_cli.get_latest_schema(subject)
     assert stored_id == sc_id
