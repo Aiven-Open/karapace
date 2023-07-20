76da93200312acfb9bfc625161289d9db92f636f
Elia Migliore <elia.migliore@aiven.io>
2023-07-20 20:26:36 +0200

    Resolve entities recursively when receiving the schema
    The previous implementation did not correctly resolve entities within a schema that contained references. This led the REST Proxy responds with a 422 error even when the message was correct.
    To address this issue, the schema resolver has been modified to ensure that schemas with references are recursively resolved and that the references are passed to the parser correctly.

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -17,7 +17,7 @@ async def test_remote_client(registry_async_client: Client) -> None:
     assert sc_id >= 0
     stored_schema, _ = await reg_cli.get_schema_for_id(sc_id)
     assert stored_schema == schema_avro, f"stored schema {stored_schema.to_dict()} is not {schema_avro.to_dict()}"
-    stored_id, stored_schema = await reg_cli.get_latest_schema(subject)
+    stored_id, stored_schema, _ = await reg_cli.get_schema(subject)
     assert stored_id == sc_id
     assert stored_schema == schema_avro
 
@@ -31,6 +31,6 @@ async def test_remote_client_tls(registry_async_client_tls: Client) -> None:
     assert sc_id >= 0
     stored_schema, _ = await reg_cli.get_schema_for_id(sc_id)
     assert stored_schema == schema_avro, f"stored schema {stored_schema.to_dict()} is not {schema_avro.to_dict()}"
-    stored_id, stored_schema = await reg_cli.get_latest_schema(subject)
+    stored_id, stored_schema = await reg_cli.get_schema(subject)
     assert stored_id == sc_id
     assert stored_schema == schema_avro
