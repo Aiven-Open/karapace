34fa7dcef5a363f21d50e12432fb8430488b7637
libretto <serge@uacoders.com>
2023-09-05 19:58:37 +0300

    fixup

--- karapace/metrics.py
+++ karapace/metrics.py
@@ -38,7 +38,6 @@ class Singleton(type):
 
 class Metrics(metaclass=Singleton):
     def __init__(self) -> None:
-        self.active = False
         self.stats_client: StatsClient
         self.is_ready = False
         self.stop_event = threading.Event()
@@ -47,17 +46,14 @@ class Metrics(metaclass=Singleton):
 
     def setup(self, config: Config) -> None:
         stats_service = config.get("stats_service")
+        if not config.get("metrics_extended"):
+            return
         if stats_service == "statsd":
             self.stats_client = StatsdClient(config=config)
         elif stats_service == "prometheus":
             self.stats_client = PrometheusClient(config=config)
         else:
-            self.active = False
             raise MetricsException('Config variable "stats_service" is not defined')
-        if config.get("metrics_extended"):  # for mypy check pass
-            self.active = True
-        if not self.active:
-            return
         with self.lock:
             if self.is_ready:
                 return
@@ -67,42 +63,42 @@ class Metrics(metaclass=Singleton):
         self.worker_thread.start()
 
     def request(self, size: int) -> None:
-        if not self.active:
+        if not self.is_ready or self.stats_client is None:
             return
         if not isinstance(self.stats_client, StatsClient):
             raise RuntimeError("no StatsClient available")
         self.stats_client.gauge("request-size", size)
 
     def response(self, size: int) -> None:
-        if not self.active:
+        if not self.is_ready or self.stats_client is None:
             return
         if not isinstance(self.stats_client, StatsClient):
             raise RuntimeError("no StatsClient available")
         self.stats_client.gauge("response-size", size)
 
     def are_we_master(self, is_master: bool) -> None:
-        if not self.active:
+        if not self.is_ready or self.stats_client is None:
             return
         if not isinstance(self.stats_client, StatsClient):
             raise RuntimeError("no StatsClient available")
         self.stats_client.gauge("master-slave-role", int(is_master))
 
     def latency(self, latency_ms: float) -> None:
-        if not self.active:
+        if not self.is_ready or self.stats_client is None:
             return
         if not isinstance(self.stats_client, StatsClient):
             raise RuntimeError("no StatsClient available")
         self.stats_client.timing("latency_ms", latency_ms)
 
     def error(self) -> None:
-        if not self.active:
+        if not self.is_ready or self.stats_client is None:
             return
         if not isinstance(self.stats_client, StatsClient):
             raise RuntimeError("no StatsClient available")
         self.stats_client.increase("error_total", 1)
 
     def connections(self) -> None:
-        if not self.active:
+        if not self.is_ready or self.stats_client is None:
             return
         if not isinstance(self.stats_client, StatsClient):
             raise RuntimeError("no StatsClient available")
@@ -124,7 +120,7 @@ class Metrics(metaclass=Singleton):
     def cleanup(self) -> None:
         if self.stats_client:
             self.stats_client.close()
-        if not self.active:
+        if not self.is_ready:
             return
         self.stop_event.set()
         if self.worker_thread.is_alive():
