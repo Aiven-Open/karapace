d8212b8cb8654c2561ee6edb2939a6600cf80587
Elia Migliore <elia.migliore@aiven.io>
2023-09-15 17:01:56 +0200

    fix: enable self-referencing schemas

--- karapace/protobuf/dependency.py
+++ karapace/protobuf/dependency.py
@@ -8,7 +8,7 @@ See LICENSE for details
 from karapace.dependency import DependencyVerifierResult
 from karapace.protobuf.known_dependency import DependenciesHardcoded, KnownDependency
 from karapace.protobuf.one_of_element import OneOfElement
-from typing import List
+from typing import List, Optional, Set
 
 
 class ProtobufDependencyVerifier:
@@ -35,13 +35,31 @@ class ProtobufDependencyVerifier:
     def add_import(self, import_name: str) -> None:
         self.import_path.append(import_name)
 
+    def is_type_declared(
+        self,
+        used_type: str,
+        declared_index: Set[str],
+        father_child_type: Optional[str],
+        used_type_with_scope: Optional[str],
+    ) -> bool:
+        return (
+            used_type in declared_index
+            or (used_type_with_scope is not None and used_type_with_scope in declared_index)
+            or (father_child_type is not None and father_child_type in declared_index)
+            or "." + used_type in declared_index
+        )
+
     def verify(self) -> DependencyVerifierResult:
         declared_index = set(self.declared_types)
         for used_type in self.used_types:
             delimiter = used_type.rfind(";")
-            used_type_with_scope = ""
+            father_child_type = None
+            used_type_with_scope = None
             if delimiter != -1:
                 used_type_with_scope = used_type[:delimiter] + "." + used_type[delimiter + 1 :]
+                father_delimiter = used_type[:delimiter].find(".")
+                if father_delimiter != -1:
+                    father_child_type = used_type[:father_delimiter] + "." + used_type[delimiter + 1 :]
                 used_type = used_type[delimiter + 1 :]
 
             if used_type in DependenciesHardcoded.index:
@@ -51,11 +69,7 @@ class ProtobufDependencyVerifier:
             if known_pkg is not None and known_pkg in self.import_path:
                 continue
 
-            if (
-                used_type in declared_index
-                or (delimiter != -1 and used_type_with_scope in declared_index)
-                or "." + used_type in declared_index
-            ):
+            if self.is_type_declared(used_type, declared_index, father_child_type, used_type_with_scope):
                 continue
 
             return DependencyVerifierResult(False, f'type "{used_type}" is not defined')
