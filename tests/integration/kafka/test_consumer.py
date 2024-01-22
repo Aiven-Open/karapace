b17003f95d960f9975ecb2d39f035f4994d1a3eb
Mátyás Kuti <matyas.kuti@aiven.io>
2024-01-22 12:54:44 +0100

    Replace async consumer with confluent-kafka one

--- tests/integration/kafka/test_consumer.py
+++ tests/integration/kafka/test_consumer.py
@@ -4,14 +4,21 @@ See LICENSE for details
 """
 from __future__ import annotations
 
-from confluent_kafka import TopicPartition
+from confluent_kafka import OFFSET_BEGINNING, OFFSET_END, TopicPartition
 from confluent_kafka.admin import NewTopic
-from kafka.errors import UnknownTopicOrPartitionError
-from karapace.kafka.consumer import KafkaConsumer
-from karapace.kafka.producer import KafkaProducer
+from confluent_kafka.error import KafkaError
+from kafka.errors import IllegalStateError, KafkaTimeoutError, UnknownTopicOrPartitionError
+from karapace.kafka.admin import KafkaAdminClient
+from karapace.kafka.consumer import AsyncKafkaConsumer, KafkaConsumer
+from karapace.kafka.producer import AsyncKafkaProducer, KafkaProducer
+from karapace.utils import Expiration
 from tests.integration.utils.kafka_server import KafkaServers
+from tests.utils import new_topic as create_new_topic
+from typing import Final
 
 import pytest
+
+POLL_TIMEOUT_S: Final = 10
 
 
 class TestPartitionsForTopic:
@@ -56,3 +63,306 @@ class TestGetWatermarkOffsets:
 
         assert beginning == 0
         assert end == 1
+
+
+class TestCommit:
+    def test_commit_message_and_offset_mutual_exclusion(
+        self,
+        consumer: KafkaConsumer,
+        producer: KafkaProducer,
+        new_topic: NewTopic,
+    ) -> None:
+        fut = producer.send(new_topic.topic)
+        producer.flush()
+        message = fut.result()
+
+        with pytest.raises(ValueError):
+            consumer.commit(message=message, offsets=[])
+
+    def test_commit_message(
+        self,
+        producer: KafkaProducer,
+        consumer: KafkaConsumer,
+        new_topic: NewTopic,
+    ) -> None:
+        consumer.subscribe([new_topic.topic])
+        first_fut = producer.send(new_topic.topic)
+        second_fut = producer.send(new_topic.topic)
+        producer.flush()
+        first_fut.result()
+        second_fut.result()
+        consumer.poll(timeout=POLL_TIMEOUT_S)
+        message = consumer.poll(timeout=POLL_TIMEOUT_S)
+
+        [topic_partition] = consumer.commit(message)
+        [committed_partition] = consumer.committed([TopicPartition(new_topic.topic, partition=0)])
+
+        assert topic_partition.topic == new_topic.topic
+        assert topic_partition.partition == 0
+        assert topic_partition.offset == 2
+        assert committed_partition.topic == new_topic.topic
+        assert committed_partition.partition == 0
+        assert committed_partition.offset == 2
+
+    def test_commit_offsets(
+        self,
+        producer: KafkaProducer,
+        consumer: KafkaConsumer,
+        new_topic: NewTopic,
+    ) -> None:
+        consumer.subscribe([new_topic.topic])
+        first_fut = producer.send(new_topic.topic)
+        second_fut = producer.send(new_topic.topic)
+        producer.flush()
+        first_fut.result()
+        second_fut.result()
+        consumer.poll(timeout=POLL_TIMEOUT_S)
+        message = consumer.poll(timeout=POLL_TIMEOUT_S)
+
+        [topic_partition] = consumer.commit(
+            offsets=[
+                TopicPartition(
+                    new_topic.topic,
+                    partition=0,
+                    offset=message.offset() + 1,
+                ),
+            ]
+        )
+        [committed_partition] = consumer.committed([TopicPartition(new_topic.topic, partition=0)])
+
+        assert topic_partition.topic == new_topic.topic
+        assert topic_partition.partition == 0
+        assert topic_partition.offset == 2
+        assert committed_partition.topic == new_topic.topic
+        assert committed_partition.partition == 0
+        assert committed_partition.offset == 2
+
+    def test_commit_raises_for_unknown_partition(
+        self,
+        consumer: KafkaConsumer,
+        new_topic: NewTopic,
+    ) -> None:
+        consumer.subscribe([new_topic.topic])
+        with pytest.raises(UnknownTopicOrPartitionError):
+            consumer.commit(offsets=[TopicPartition(new_topic.topic, partition=99, offset=0)])
+
+
+class TestCommitted:
+    def test_committed_raises_on_timeout(self, consumer: KafkaConsumer) -> None:
+        with pytest.raises(KafkaTimeoutError):
+            consumer.committed([], timeout=0.00001)
+
+
+class TestSubscribe:
+    def _poll_until_no_message(self, consumer: KafkaConsumer) -> None:
+        """Polls until there is no message returned.
+
+        When verifying subscriptions this can be used to wait until the topics
+        subscribed to are all ready. Until a topic is not ready, a message is
+        returned with an error indicating that certain topics are not ready to
+        be consumed yet.
+        """
+        msg = consumer.poll(timeout=POLL_TIMEOUT_S)
+        while msg is not None:
+            msg = consumer.poll(timeout=POLL_TIMEOUT_S)
+
+    def test_subscription_is_recorded(
+        self,
+        admin_client: KafkaAdminClient,
+        consumer: KafkaConsumer,
+        new_topic: NewTopic,
+    ) -> None:
+        prefix = "subscribe"
+        topics = [create_new_topic(admin_client, prefix=prefix) for _ in range(3)]
+
+        consumer.subscribe(topics=[new_topic.topic], patterns=[f"{prefix}.*"])
+        self._poll_until_no_message(consumer)
+
+        assert consumer.subscription() == frozenset(topics + [new_topic.topic])
+
+    def test_unsubscribe_empties_subscription(
+        self,
+        admin_client: KafkaAdminClient,
+        consumer: KafkaConsumer,
+        new_topic: NewTopic,
+    ) -> None:
+        prefix = "unsubscribe"
+        _ = [create_new_topic(admin_client, prefix=prefix) for _ in range(3)]
+        consumer.subscribe(topics=[new_topic.topic], patterns=[f"{prefix}.*"])
+        self._poll_until_no_message(consumer)
+
+        consumer.unsubscribe()
+
+        self._poll_until_no_message(consumer)
+
+        assert consumer.subscription() == frozenset()
+
+    def test_resubscribe_modifies_subscription(
+        self,
+        admin_client: KafkaAdminClient,
+        consumer: KafkaConsumer,
+        new_topic: NewTopic,
+    ) -> None:
+        prefix = "resubscribe"
+        _ = [create_new_topic(admin_client, prefix=prefix) for _ in range(3)]
+        consumer.subscribe(topics=[new_topic.topic], patterns=[f"{prefix}.*"])
+        self._poll_until_no_message(consumer)
+
+        consumer.subscribe(topics=[new_topic.topic])
+
+        self._poll_until_no_message(consumer)
+
+        assert consumer.subscription() == frozenset([new_topic.topic])
+
+
+class TestAssign:
+    def test_assign(self, consumer: KafkaConsumer, producer: KafkaProducer, new_topic: NewTopic) -> None:
+        first_fut = producer.send(new_topic.topic)
+        second_fut = producer.send(new_topic.topic, value=b"message-value")
+        producer.flush()
+        first_fut.result()
+        second_fut.result()
+        consumer.assign([TopicPartition(new_topic.topic, partition=0, offset=1)])
+
+        [assigned_partition] = consumer.assignment()
+        first_message = consumer.poll(POLL_TIMEOUT_S)
+        second_message = consumer.poll(POLL_TIMEOUT_S)
+
+        assert first_message.offset() == 1
+        assert first_message.topic() == new_topic.topic
+        assert first_message.partition() == 0
+        assert first_message.key() is None
+        assert first_message.value() == b"message-value"
+        assert second_message is None
+
+        assert assigned_partition.topic == new_topic.topic
+        assert assigned_partition.partition == 0
+        assert assigned_partition.offset == 1
+
+    def test_assign_raises_illegal_state_after_subscribe(
+        self,
+        consumer: KafkaConsumer,
+        new_topic: NewTopic,
+    ) -> None:
+        consumer.subscribe([new_topic.topic])
+        consumer.poll(timeout=POLL_TIMEOUT_S)
+
+        with pytest.raises(IllegalStateError):
+            consumer.assign([TopicPartition("some-topic", 0)])
+
+
+class TestSeek:
+    def test_seek(self, consumer: KafkaConsumer, producer: KafkaProducer, new_topic: NewTopic) -> None:
+        consumer.subscribe([new_topic.topic])
+        fut = producer.send(new_topic.topic, value=b"message-value")
+        producer.flush()
+        fut.result()
+
+        message = consumer.poll(timeout=POLL_TIMEOUT_S)
+        consumer.seek(TopicPartition(new_topic.topic, partition=0, offset=OFFSET_BEGINNING))
+        same_message = consumer.poll(timeout=POLL_TIMEOUT_S)
+
+        assert message.offset() == same_message.offset()
+        assert message.topic() == same_message.topic()
+        assert message.partition() == same_message.partition()
+        assert message.key() is None
+        assert same_message.key() is None
+        assert message.value() == same_message.value()
+
+    def test_seek_unassigned_partition_raises(self, consumer: KafkaConsumer, new_topic: NewTopic) -> None:
+        with pytest.raises(UnknownTopicOrPartitionError):
+            consumer.seek(TopicPartition(new_topic.topic, partition=0, offset=OFFSET_END))
+
+
+class TestAsyncPoll:
+    async def test_async_poll(
+        self,
+        asyncproducer: AsyncKafkaProducer,
+        asyncconsumer: AsyncKafkaConsumer,
+        new_topic: NewTopic,
+    ) -> None:
+        await asyncconsumer.subscribe([new_topic.topic])
+        aiofut = await asyncproducer.send(new_topic.topic)
+        await aiofut
+
+        message = await asyncconsumer.poll(timeout=POLL_TIMEOUT_S)
+
+        assert message.offset() == 0
+        assert message.topic() == new_topic.topic
+        assert message.partition() == 0
+        assert message.key() is None
+        assert message.value() is None
+
+    async def test_async_poll_no_message(self, asyncconsumer: AsyncKafkaConsumer, new_topic: NewTopic) -> None:
+        await asyncconsumer.subscribe([new_topic.topic])
+
+        message = await asyncconsumer.poll(timeout=1)
+
+        assert message is None
+
+    async def test_async_poll_unknown_topic(self, asyncconsumer: AsyncKafkaConsumer) -> None:
+        await asyncconsumer.subscribe(["nonexistent"])
+
+        message = await asyncconsumer.poll(timeout=POLL_TIMEOUT_S)
+
+        assert message.topic() == "nonexistent"
+        assert message.partition() == 0
+        assert message.error() is not None
+        assert message.error().code() == KafkaError.UNKNOWN_TOPIC_OR_PART
+        assert message.offset() is None
+        assert message.key() is None
+        assert message.value().decode() == message.error().str()
+
+    async def test_async_poll_existing_topic_and_unknown_topic_pattern(
+        self,
+        asyncproducer: AsyncKafkaProducer,
+        asyncconsumer: AsyncKafkaConsumer,
+        new_topic: NewTopic,
+    ) -> None:
+        await asyncconsumer.subscribe(topics=[new_topic.topic], patterns=["nonexistent.*"])
+        aiofut = await asyncproducer.send(new_topic.topic, value="message-value")
+        sent_message = await aiofut
+
+        message = await asyncconsumer.poll(timeout=POLL_TIMEOUT_S)
+        while message.error() is not None:
+            assert message.topic() in ("^nonexistent.*", new_topic.topic)
+            assert message.partition() == 0
+            assert message.error().code() == KafkaError.UNKNOWN_TOPIC_OR_PART
+            assert message.offset() is None
+            assert message.key() is None
+            assert message.value().decode() == message.error().str()
+            message = await asyncconsumer.poll(timeout=POLL_TIMEOUT_S)
+
+        message_none = await asyncconsumer.poll(timeout=POLL_TIMEOUT_S)
+
+        assert message.key() == sent_message.key()
+        assert message.value() == sent_message.value()
+
+        assert message_none is None
+
+
+async def test_pattern_subscription_async(
+    admin_client: KafkaAdminClient,
+    asyncproducer: AsyncKafkaProducer,
+    asyncconsumer: AsyncKafkaConsumer,
+) -> None:
+    prefix = "patterntest"
+    number_of_topics = 3
+    topics = [create_new_topic(admin_client, prefix=prefix) for _ in range(number_of_topics)]
+    await asyncconsumer.subscribe(patterns=[f"{prefix}.*"])
+    for i, topic in enumerate(topics):
+        aiofut = await asyncproducer.send(topic, value=f"{i}-value")
+        await aiofut
+
+    messages = []
+    expiration = Expiration.from_timeout(30)
+    while len(messages) != 3:
+        expiration.raise_timeout_if_expired(
+            "Timeout elapsed waiting for messages from topic pattern. Only received {messages}",
+            messages=messages,
+        )
+        message = await asyncconsumer.poll(timeout=POLL_TIMEOUT_S)
+        if message is not None and message.error() is None:
+            messages.append(message)
+
+    assert sorted(message.value().decode() for message in messages) == [f"{i}-value" for i in range(number_of_topics)]
--- tests/integration/test_rest_consumer.py
+++ tests/integration/test_rest_consumer.py
@@ -11,6 +11,7 @@ from tests.utils import (
     repeat_until_successful_request,
     REST_HEADERS,
     schema_data,
+    wait_for_topics,
 )
 
 import base64
@@ -87,7 +88,7 @@ async def test_subscription(rest_async_client, admin_client, producer, trail):
     topic_name = new_topic(admin_client)
     instance_id = await new_consumer(rest_async_client, group_name, fmt="binary", trail=trail)
     sub_path = f"/consumers/{group_name}/instances/{instance_id}/subscription{trail}"
-    consume_path = f"/consumers/{group_name}/instances/{instance_id}/records{trail}?timeout=1000"
+    consume_path = f"/consumers/{group_name}/instances/{instance_id}/records{trail}?timeout=5000"
     res = await rest_async_client.get(sub_path, headers=header)
     assert res.ok
     data = res.json()
@@ -119,6 +120,7 @@ async def test_subscription(rest_async_client, admin_client, producer, trail):
     # one pattern sub will get all 3
     prefix = f"{hash(random.random())}"
     pattern_topics = [new_topic(admin_client, prefix=f"{prefix}{i}") for i in range(3)]
+    await wait_for_topics(rest_async_client, topic_names=pattern_topics, timeout=20, sleep=1)
     res = await rest_async_client.post(sub_path, json={"topic_pattern": f"{prefix}.*"}, headers=REST_HEADERS["json"])
     assert res.ok
 
@@ -175,6 +177,7 @@ async def test_seek(rest_async_client, admin_client, trail):
     assign_payload = {"partitions": [{"topic": topic_name, "partition": 0}]}
     res = await rest_async_client.post(assign_path, headers=REST_HEADERS["json"], json=assign_payload)
     assert res.ok
+    await wait_for_topics(rest_async_client, topic_names=[topic_name], timeout=20, sleep=1)
     seek_payload = {"offsets": [{"topic": topic_name, "partition": 0, "offset": 10}]}
     res = await rest_async_client.post(seek_path, json=seek_payload, headers=REST_HEADERS["json"])
     assert res.ok, f"Unexpected status for {res}"
@@ -262,7 +265,7 @@ async def test_consume(rest_async_client, admin_client, producer, trail):
         instance_id = await new_consumer(rest_async_client, group_name, fmt=fmt, trail=trail)
         assign_path = f"/consumers/{group_name}/instances/{instance_id}/assignments{trail}"
         seek_path = f"/consumers/{group_name}/instances/{instance_id}/positions/beginning{trail}"
-        consume_path = f"/consumers/{group_name}/instances/{instance_id}/records{trail}?timeout=1000"
+        consume_path = f"/consumers/{group_name}/instances/{instance_id}/records{trail}?timeout=5000"
         topic_name = new_topic(admin_client)
         assign_payload = {"partitions": [{"topic": topic_name, "partition": 0}]}
         res = await rest_async_client.post(assign_path, json=assign_payload, headers=header)
@@ -350,7 +353,7 @@ async def test_publish_consume_avro(rest_async_client, admin_client, trail, sche
     group_name = "e2e_group"
     instance_id = await new_consumer(rest_async_client, group_name, fmt=schema_type, trail=trail)
     assign_path = f"/consumers/{group_name}/instances/{instance_id}/assignments{trail}"
-    consume_path = f"/consumers/{group_name}/instances/{instance_id}/records{trail}?timeout=1000"
+    consume_path = f"/consumers/{group_name}/instances/{instance_id}/records{trail}?timeout=5000"
     tn = new_topic(admin_client)
     assign_payload = {"partitions": [{"topic": tn, "partition": 0}]}
     res = await rest_async_client.post(assign_path, json=assign_payload, headers=header)
@@ -393,7 +396,7 @@ async def test_consume_grafecul_deserialization_error_handling(rest_async_client
     res = await rest_async_client.post(assign_path, json=assign_payload, headers=headers)
     assert res.ok
 
-    consume_path = f"/consumers/{group_name}/instances/{instance_id}/records?timeout=1000"
+    consume_path = f"/consumers/{group_name}/instances/{instance_id}/records?timeout=5000"
     resp = await rest_async_client.get(consume_path, headers=headers)
     if fmt == "binary":
         assert resp.status_code == 200, f"Expected 200 response: {resp}"
