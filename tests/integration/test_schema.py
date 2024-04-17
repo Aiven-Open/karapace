b8764720710c630e9d4872bd56c45b4b293db504
Tommi Vainikainen <tvainika@aiven.io>
2024-04-17 13:34:59 +0300

    Fix error message formatting variable expansion
    Fix formatting of error message variable expansion if subject level
    compatibility mode is not configured.

--- tests/integration/test_schema.py
+++ tests/integration/test_schema.py
@@ -2103,9 +2103,7 @@ async def test_config(registry_async_client: Client, trail: str) -> None:
     res = await registry_async_client.get(f"config/{subject_1}{trail}")
     assert res.status_code == 404
     assert res.json()["error_code"] == 40408
-    assert res.json()["message"] == SchemaErrorMessages.SUBJECT_LEVEL_COMPATIBILITY_NOT_CONFIGURED_FMT.value.format(
-        subject=subject_1
-    )
+    assert res.json()["message"] == f"Subject '{subject_1}' does not have subject-level compatibility configured"
 
     res = await registry_async_client.put(f"config/{subject_1}{trail}", json={"compatibility": "FULL"})
     assert res.status_code == 200
@@ -2125,9 +2123,7 @@ async def test_config(registry_async_client: Client, trail: str) -> None:
     res = await registry_async_client.get(f"config/{subject_1}{trail}")
     assert res.status_code == 404
     assert res.json()["error_code"] == 40408
-    assert res.json()["message"] == SchemaErrorMessages.SUBJECT_LEVEL_COMPATIBILITY_NOT_CONFIGURED_FMT.value.format(
-        subject=subject_1
-    )
+    assert res.json()["message"] == f"Subject '{subject_1}' does not have subject-level compatibility configured"
 
     # It's possible to add a config to a subject that doesn't exist yet
     subject_2 = subject_name_factory()
