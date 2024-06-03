7f0b3402972de31db8aec4554954e5d1459f6ef5
Emmanuel Evbuomwan <emmanuel.evbuomwan@aiven.io>
2024-06-03 14:13:23 +0200

    feature,tests: added `Version` class
    - this will replace the previously added `SchemaVersionManager`
    - it will handle the resolution and validation of versions
    - easy work between numeric and string version tags, keeping only the numeric version internally

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -5,16 +5,16 @@ Copyright (c) 2024 Aiven Ltd
 See LICENSE for details
 """
 
-from karapace.utils import intstr_conversion_guard
+from karapace.utils import intstr_version_guard
 
 import pytest
 
 
-def test_intstr_conversion_guard():
+def test_intstr_version_guard():
     class RaiseMe(Exception):
         pass
 
-    @intstr_conversion_guard(to_raise=RaiseMe)
+    @intstr_version_guard(to_raise=RaiseMe)
     def raise_value_error():
         int("not a number")
 
