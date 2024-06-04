f14df72c9bb96220e25784c9d7c51efb999d5766
Elia Migliore <elia.migliore@aiven.io>
2024-06-04 17:32:29 +0200

    WIP

--- tests/integration/test_metrics.py
+++ tests/integration/test_metrics.py
@@ -1,10 +1,9 @@
-import json
 from typing import Callable, AsyncContextManager
 
 from karapace.client import Client
 from karapace.config import ConfigDefaults
-from tests.utils import create_subject_name_factory, create_schema_name_factory
 from tests.integration.conftest import fixture_registry_async_client_custom_config, fixture_registry_cluster_with_custom_config  # noqa
+from prometheus_client.parser import text_string_to_metric_families
 import requests
 
 
@@ -17,4 +16,6 @@ async def test_metrics_are_produced(
             "prometheus_port": 9090,
         }
     ) as client:
+        metrics_available = requests.get("http://127.0.0.1:9090/metrics").text
+        print(text_string_to_metric_families(metrics_available))
         assert "karapace_schema_reader_records_processed_total" in requests.get("http://localhost:9090/api/v1/label/__name__/values").text
