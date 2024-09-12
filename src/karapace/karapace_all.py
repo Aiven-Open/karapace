edca9addc40481487941284c2977f7ec156b3f6b
Jarkko Jaakola <jarkko.jaakola@aiven.io>
2024-09-12 17:14:57 +0300

    feat: add systemd journal logging configuration

--- src/karapace/karapace_all.py
+++ src/karapace/karapace_all.py
@@ -2,10 +2,12 @@
 Copyright (c) 2023 Aiven Ltd
 See LICENSE for details
 """
+from __future__ import annotations
+
 from aiohttp.web_log import AccessLogger
 from contextlib import closing
 from karapace import version as karapace_version
-from karapace.config import read_config
+from karapace.config import Config, read_config
 from karapace.instrumentation.prometheus import PrometheusInstrumentation
 from karapace.kafka_rest_apis import KafkaRest
 from karapace.rapu import RestApp
@@ -21,6 +23,38 @@ class KarapaceAll(KafkaRest, KarapaceSchemaRegistryController):
     pass
 
 
+def _configure_logging(*, config: Config) -> None:
+    log_level = config.get("log_level", "DEBUG")
+    log_format = config.get("log_format", "%(name)-20s\t%(threadName)s\t%(levelname)-8s\t%(message)s")
+
+    root_handler: logging.Handler | None = None
+    log_handler = config.get("log_handler", None)
+    if "systemd" == log_handler:
+        from systemd import journal
+
+        root_handler = journal.JournalHandler(SYSLOG_IDENTIFIER="karapace")
+    elif "stdout" == log_handler or log_handler is None:
+        root_handler = logging.StreamHandler(stream=sys.stdout)
+    else:
+        logging.basicConfig(level=logging.INFO, format=log_format)
+        logging.getLogger().setLevel(log_level)
+        logging.warning("Log handler %s not recognized, root handler not set.", log_handler)
+
+    if root_handler is not None:
+        root_handler.setFormatter(logging.Formatter(log_format))
+        root_handler.setLevel(log_level)
+        root_handler.set_name(name="karapace")
+        logging.root.addHandler(root_handler)
+
+    logging.root.setLevel(log_level)
+
+    if config.get("access_logs_debug") is True:
+        config["access_log_class"] = DebugAccessLogger
+        logging.getLogger("aiohttp.access").setLevel(logging.DEBUG)
+    else:
+        config["access_log_class"] = AccessLogger
+
+
 def main() -> int:
     parser = argparse.ArgumentParser(prog="karapace", description="Karapace: Your Kafka essentials in one tool")
     parser.add_argument("--version", action="version", help="show program version", version=karapace_version.__version__)
@@ -30,13 +64,7 @@ def main() -> int:
     with closing(arg.config_file):
         config = read_config(arg.config_file)
 
-    logging.basicConfig(level=logging.INFO, format=config["log_format"])
-    logging.getLogger().setLevel(config["log_level"])
-    if config.get("access_logs_debug") is True:
-        config["access_log_class"] = DebugAccessLogger
-        logging.getLogger("aiohttp.access").setLevel(logging.DEBUG)
-    else:
-        config["access_log_class"] = AccessLogger
+    _configure_logging(config=config)
 
     app: RestApp
     if config["karapace_rest"] and config["karapace_registry"]:
