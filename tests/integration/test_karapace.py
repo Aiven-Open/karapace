ccbc33f03a5a24f907129509ef03727d01e7d8fe
Jarkko Jaakola <jarkko.jaakola@aiven.io>
2024-11-15 15:34:37 +0200

    fix: corrected test_karapace.py test

--- tests/integration/test_karapace.py
+++ tests/integration/test_karapace.py
@@ -4,13 +4,12 @@ See LICENSE for details
 """
 from collections.abc import Iterator
 from contextlib import closing, contextmanager, ExitStack
-from karapace.config import set_config_defaults
+from karapace.config import Config, write_env_file
 from pathlib import Path
 from tests.integration.utils.kafka_server import KafkaServers
 from tests.integration.utils.process import stop_process
 from tests.utils import popen_karapace_all
 
-import json
 import socket
 
 
@@ -36,18 +35,20 @@ def test_regression_server_must_exit_on_exception(
     with ExitStack() as stack:
         port = stack.enter_context(allocate_port_no_reuse())
 
-        config = set_config_defaults(
-            {
-                "bootstrap_uri": kafka_servers.bootstrap_servers,
-                "karapace_registry": True,
-                "port": port,
-            }
-        )
-        config_path = tmp_path / "karapace.json"
+        config = Config()
+        config.bootstrap_uri = kafka_servers.bootstrap_servers[0]
+        config.port = port
+        config.karapace_registry = True
+
+        env_path = tmp_path / "karapace.env"
+
+        print(f"{tmp_path}/karapace.log")
+        print(f"{tmp_path}/karapace.err")
 
         logfile = stack.enter_context((tmp_path / "karapace.log").open("w"))
         errfile = stack.enter_context((tmp_path / "karapace.err").open("w"))
-        config_path.write_text(json.dumps(config))
-        process = popen_karapace_all(config_path, logfile, errfile)
+
+        write_env_file(dot_env_path=env_path, config=config)
+        process = popen_karapace_all(host=config.host, port=port, env_path=env_path, stdout=logfile, stderr=errfile)
         stack.callback(stop_process, process)  # make sure to stop the process if the test fails
         assert process.wait(timeout=10) != 0, "Process should have exited with an error, port is already is use"
