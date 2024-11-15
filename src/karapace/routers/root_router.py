e5ebb45d4faa9483e6de811d330820a9245417b0
Jarkko Jaakola <jarkko.jaakola@aiven.io>
2024-11-15 20:46:59 +0200

    fix: add missing root router file

---
+++ src/karapace/routers/root_router.py
@@ -0,0 +1,17 @@
+"""
+Copyright (c) 2024 Aiven Ltd
+See LICENSE for details
+"""
+
+from fastapi import APIRouter
+
+
+root_router = APIRouter(
+    tags=["root"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@root_router.get("/")
+async def compatibility_post() -> dict:
+    return {}
