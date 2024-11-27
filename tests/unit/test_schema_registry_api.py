0850e03ff9a0f9bc05c5906a046af34be0b690b9
Emmanuel Evbuomwan <emmanuel.evbuomwan@aiven.io>
2024-11-27 11:56:48 +0100

    feat: run unit-tests in the cli docker container
    - we replace use the karapace container to load the default config

--- tests/unit/test_schema_registry_api.py
+++ tests/unit/test_schema_registry_api.py
@@ -2,65 +2,83 @@
 Copyright (c) 2023 Aiven Ltd
 See LICENSE for details
 """
-from aiohttp.test_utils import TestClient, TestServer
-from karapace.config import DEFAULTS, set_config_defaults
+from fastapi.exceptions import HTTPException
 from karapace.rapu import HTTPResponse
+from karapace.schema_models import SchemaType, ValidatedTypedSchema
 from karapace.schema_reader import KafkaSchemaReader
 from karapace.schema_registry import KarapaceSchemaRegistry
 from karapace.schema_registry_apis import KarapaceSchemaRegistryController
 from karapace.typing import PrimaryInfo
 from unittest.mock import ANY, AsyncMock, Mock, patch, PropertyMock
+from schema_registry.container import SchemaRegistryContainer
+from unittest.mock import Mock, patch, PropertyMock
 
 import asyncio
+import json
 import pytest
 
+TYPED_AVRO_SCHEMA = ValidatedTypedSchema.parse(
+    SchemaType.AVRO,
+    json.dumps(
+        {
+            "namespace": "io.aiven.data",
+            "name": "Test",
+            "type": "record",
+            "fields": [
+                {
+                    "name": "attr1",
+                    "type": ["null", "string"],
+                }
+            ],
+        }
+    ),
+)
 
-async def test_validate_schema_request_body() -> None:
-    controller = KarapaceSchemaRegistryController(config=set_config_defaults(DEFAULTS))
 
-    controller._validate_schema_request_body(  # pylint: disable=W0212
-        "application/json", {"schema": "{}", "schemaType": "JSON", "references": [], "metadata": {}, "ruleSet": {}}
+async def test_validate_schema_request_body(schema_registry_container: SchemaRegistryContainer) -> None:
+    schema_registry_container.schema_registry_controller()._validate_schema_type(  # pylint: disable=W0212
+        {"schema": "{}", "schemaType": "JSON", "references": [], "metadata": {}, "ruleSet": {}}
     )
 
-    with pytest.raises(HTTPResponse) as exc_info:
-        controller._validate_schema_request_body(  # pylint: disable=W0212
-            "application/json",
-            {"schema": "{}", "schemaType": "JSON", "references": [], "unexpected_field_name": {}, "ruleSet": {}},
+    with pytest.raises(HTTPException) as exc_info:
+        schema_registry_container.schema_registry_controller()._validate_schema_type(  # pylint: disable=W0212
+            {"schema": "{}", "schemaType": "DOES_NOT_EXIST", "references": [], "unexpected_field_name": {}, "ruleSet": {}},
         )
-    assert exc_info.type is HTTPResponse
-    assert str(exc_info.value) == "HTTPResponse 422"
+    assert exc_info.type is HTTPException
+    assert str(exc_info.value) == "422: {'error_code': 422, 'message': 'Invalid schemaType DOES_NOT_EXIST'}"
 
 
-async def test_forward_when_not_ready() -> None:
-    with patch("karapace.schema_registry_apis.KarapaceSchemaRegistry") as schema_registry_class:
+async def test_forward_when_not_ready(schema_registry_container: SchemaRegistryContainer) -> None:
+    with patch("karapace.container.KarapaceSchemaRegistry") as schema_registry_class:
         schema_reader_mock = Mock(spec=KafkaSchemaReader)
-        ready_property_mock = PropertyMock(return_value=lambda: False)
-        schema_registry = AsyncMock(spec=KarapaceSchemaRegistry)
+        ready_property_mock = PropertyMock(return_value=False)
         type(schema_reader_mock).ready = ready_property_mock
-        schema_registry.schema_reader = schema_reader_mock
-        schema_registry_class.return_value = schema_registry
+        schema_registry_class.schema_reader = schema_reader_mock
 
+        schema_registry_class.schemas_get.return_value = TYPED_AVRO_SCHEMA
         schema_registry.get_master.return_value = PrimaryInfo(primary=False, primary_url="http://primary-url")
 
         close_future_result = asyncio.Future()
         close_future_result.set_result(True)
         close_func = Mock()
         close_func.return_value = close_future_result
-        schema_registry.close = close_func
+        schema_registry_class.close = close_func
 
-        controller = KarapaceSchemaRegistryController(config=set_config_defaults(DEFAULTS))
+        schema_registry_container.karapace_container().schema_registry = schema_registry_class
+        controller = schema_registry_container.schema_registry_controller()
+        controller.schema_registry = schema_registry_class
+
         mock_forward_func_future = asyncio.Future()
         mock_forward_func_future.set_exception(HTTPResponse({"mock": "response"}))
         mock_forward_func = Mock()
         mock_forward_func.return_value = mock_forward_func_future
         controller._forward_request_remote = mock_forward_func  # pylint: disable=protected-access
 
-        test_server = TestServer(controller.app)
-        async with TestClient(test_server) as client:
-            await client.get("/schemas/ids/1", headers={"Content-Type": "application/json"})
-
-            ready_property_mock.assert_called_once()
-            schema_registry.get_master.assert_called_once()
-            mock_forward_func.assert_called_once_with(
-                request=ANY, body=None, url="http://primary-url/schemas/ids/1", content_type="application/json", method="GET"
-            )
+        assert await controller.schemas_get(
+            schema_id=1,
+            include_subjects=False,
+            fetch_max_id=False,
+            format_serialized="",
+            user=None,
+            authorizer=None,
+        )
