5a736001de2e1f8ac469e90f1da6a16e3ce3d294
Emmanuel Evbuomwan <emmanuel.evbuomwan@aiven.io>
2024-12-16 21:58:18 +0100

    otel: use dedicated container
    - we also build a custom resource

--- src/schema_registry/telemetry/container.py
+++ src/schema_registry/telemetry/container.py
@@ -4,10 +4,28 @@ See LICENSE for details
 """
 
 from dependency_injector import containers, providers
+from karapace.config import Config
+from karapace.container import KarapaceContainer
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.semconv.attributes import telemetry_attributes as T
 from schema_registry.telemetry.tracer import Tracer
 
 
+def create_tracing_resource(config: Config) -> Resource:
+    return Resource.create(
+        {
+            "service.name": config.telemetry.resource_service_name,
+            "service.instance.id": config.telemetry.resource_service_instance_id,
+            T.TELEMETRY_SDK_NAME: config.telemetry.resource_telemetry_sdk_name,
+            T.TELEMETRY_SDK_LANGUAGE: config.telemetry.resource_telemetry_sdk_language,
+            T.TELEMETRY_SDK_VERSION: config.telemetry.resource_telemetry_sdk_version,
+        }
+    )
+
+
 class TelemetryContainer(containers.DeclarativeContainer):
-    tracer_provider = providers.Singleton(TracerProvider)
+    karapace_container = providers.Container(KarapaceContainer)
+    tracing_resource = providers.Factory(create_tracing_resource, config=karapace_container.config)
+    tracer_provider = providers.Singleton(TracerProvider, resource=tracing_resource)
     tracer = providers.Singleton(Tracer)
