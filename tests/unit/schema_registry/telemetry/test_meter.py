aad948e5c43b6cf75690b4daf79def53518aab2f
Emmanuel Evbuomwan <emmanuel.evbuomwan@aiven.io>
2024-12-19 10:45:16 +0100

    metering: add middleware for HTTP metrics

--- tests/unit/schema_registry/telemetry/test_meter.py
+++ tests/unit/schema_registry/telemetry/test_meter.py
@@ -5,6 +5,7 @@ Copyright (c) 2024 Aiven Ltd
 See LICENSE for details
 """
 
+from karapace.config import KarapaceTelemetry
 from karapace.container import KarapaceContainer
 from schema_registry.telemetry.meter import Meter
 from unittest.mock import patch
@@ -16,30 +17,35 @@ def test_meter(karapace_container: KarapaceContainer):
         mock_metrics.get_meter_provider.return_value.get_meter.assert_called_once_with("Karapace.meter")
 
 
-def test_get_metric_reader_with_otel_endpoint(karapace_container: KarapaceContainer) -> None:
+def test_get_metric_reader_without_otel_endpoint(karapace_container: KarapaceContainer) -> None:
+    config = karapace_container.config().set_config_defaults(
+        new_config={"telemetry": KarapaceTelemetry(otel_endpoint_url=None)}
+    )
     with (
-        patch("schema_registry.telemetry.meter.OTLPMetricExporter") as mock_otlp_exporter,
+        patch("schema_registry.telemetry.meter.ConsoleMetricExporter") as mock_console_exporter,
         patch("schema_registry.telemetry.meter.PeriodicExportingMetricReader") as mock_periodic_exporting_metric_reader,
     ):
-        karapace_container.config().telemetry.otel_endpoint_url = "http://otel:4317"
-        reader = Meter.get_metric_reader(config=karapace_container.config())
-        mock_otlp_exporter.assert_called_once_with(endpoint="http://otel:4317")
+        reader = Meter.get_metric_reader(config=config)
+        mock_console_exporter.assert_called_once()
         mock_periodic_exporting_metric_reader.assert_called_once_with(
-            exporter=mock_otlp_exporter.return_value,
+            exporter=mock_console_exporter.return_value,
             export_interval_millis=10000,
         )
         assert reader is mock_periodic_exporting_metric_reader.return_value
 
 
-def test_get_metric_reader_without_otel_endpoint(karapace_container: KarapaceContainer) -> None:
+def test_get_metric_reader_with_otel_endpoint(karapace_container: KarapaceContainer) -> None:
+    config = karapace_container.config().set_config_defaults(
+        new_config={"telemetry": KarapaceTelemetry(otel_endpoint_url="http://otel:4317")}
+    )
     with (
-        patch("schema_registry.telemetry.meter.ConsoleMetricExporter") as mock_console_exporter,
+        patch("schema_registry.telemetry.meter.OTLPMetricExporter") as mock_otlp_exporter,
         patch("schema_registry.telemetry.meter.PeriodicExportingMetricReader") as mock_periodic_exporting_metric_reader,
     ):
-        reader = Meter.get_metric_reader(config=karapace_container.config())
-        mock_console_exporter.assert_called_once()
+        reader = Meter.get_metric_reader(config=config)
+        mock_otlp_exporter.assert_called_once_with(endpoint="http://otel:4317")
         mock_periodic_exporting_metric_reader.assert_called_once_with(
-            exporter=mock_console_exporter.return_value,
+            exporter=mock_otlp_exporter.return_value,
             export_interval_millis=10000,
         )
         assert reader is mock_periodic_exporting_metric_reader.return_value
