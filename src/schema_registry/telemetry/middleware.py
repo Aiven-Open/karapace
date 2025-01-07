fb19e9c39dd01e6908f84a20458fb69013cc24e8
Jarkko Jaakola <jarkko.jaakola@aiven.io>
2025-01-07 13:04:40 +0200

    fix: use server host and port instead of host header values

--- src/schema_registry/telemetry/middleware.py
+++ src/schema_registry/telemetry/middleware.py
@@ -7,6 +7,8 @@ from collections.abc import Awaitable, Callable
 from dependency_injector.wiring import inject, Provide
 from fastapi import FastAPI, Request, Response
 from opentelemetry.trace import SpanKind
+from karapace.config import Config
+from karapace.container import KarapaceContainer
 from schema_registry.telemetry.container import TelemetryContainer
 from schema_registry.telemetry.tracer import Tracer
 
@@ -20,10 +22,11 @@ async def telemetry_middleware(
     request: Request,
     call_next: Callable[[Request], Awaitable[Response]],
     tracer: Tracer = Provide[TelemetryContainer.tracer],
+    config: Config = Provide[KarapaceContainer.config],
 ) -> Response:
     resource = request.url.path.split("/")[1]
     with tracer.get_tracer().start_as_current_span(name=f"{request.method}: /{resource}", kind=SpanKind.SERVER) as span:
-        tracer.update_span_with_request(request=request, span=span)
+        tracer.update_span_with_request(request=request, span=span, config=config)
         response: Response = await call_next(request)
         tracer.update_span_with_response(response=response, span=span)
         return response
