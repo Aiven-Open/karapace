314f6422af8db83783f9ca890c49335bcd2e6788
Jarkko Jaakola <jarkko.jaakola@aiven.io>
2025-01-09 14:47:58 +0200

    fix: max http request size for Karapace REST

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -5,55 +5,37 @@ Copyright (c) 2023 Aiven Ltd
 See LICENSE for details
 """
 
+from karapace.config import Config
 from karapace.constants import DEFAULT_AIOHTTP_CLIENT_MAX_SIZE, DEFAULT_PRODUCER_MAX_REQUEST
-from karapace.container import KarapaceContainer
 
 
-def test_http_request_max_size(karapace_container: KarapaceContainer) -> None:
-    config = karapace_container.config().set_config_defaults(
-        {
-            "karapace_rest": False,
-            "producer_max_request_size": DEFAULT_PRODUCER_MAX_REQUEST + 1024,
-        }
-    )
-    assert config.http_request_max_size == DEFAULT_AIOHTTP_CLIENT_MAX_SIZE
+def test_http_request_max_size() -> None:
+    config = Config()
+    config.karapace_rest = False
+    config.producer_max_request_size = DEFAULT_PRODUCER_MAX_REQUEST + 1024
+    assert config.get_max_request_size() == DEFAULT_AIOHTTP_CLIENT_MAX_SIZE
 
-    config = karapace_container.config().set_config_defaults(
-        {
-            "karapace_rest": False,
-            "http_request_max_size": 1024,
-        }
-    )
-    assert config.http_request_max_size == 1024
+    config = Config()
+    config.karapace_rest = False
+    config.http_request_max_size = 1024
+    assert config.get_max_request_size() == 1024
 
-    config = karapace_container.config().set_config_defaults(
-        {
-            "karapace_rest": True,
-        }
-    )
-    assert config.http_request_max_size == DEFAULT_AIOHTTP_CLIENT_MAX_SIZE
+    config = Config()
+    config.karapace_rest = True
+    assert config.get_max_request_size() == DEFAULT_AIOHTTP_CLIENT_MAX_SIZE
 
-    config = karapace_container.config().set_config_defaults(
-        {
-            "karapace_rest": True,
-            "producer_max_request_size": 1024,
-        }
-    )
-    assert config.http_request_max_size == DEFAULT_AIOHTTP_CLIENT_MAX_SIZE
+    config = Config()
+    config.karapace_rest = True
+    config.producer_max_request_size = 1024
+    assert config.get_max_request_size() == DEFAULT_AIOHTTP_CLIENT_MAX_SIZE
 
-    config = karapace_container.config().set_config_defaults(
-        {
-            "karapace_rest": True,
-            "producer_max_request_size": DEFAULT_PRODUCER_MAX_REQUEST + 1024,
-        }
-    )
-    assert config.http_request_max_size == DEFAULT_PRODUCER_MAX_REQUEST + 1024 + DEFAULT_AIOHTTP_CLIENT_MAX_SIZE
+    config = Config()
+    config.karapace_rest = True
+    config.producer_max_request_size = DEFAULT_PRODUCER_MAX_REQUEST + 1024
+    assert config.get_max_request_size() == DEFAULT_PRODUCER_MAX_REQUEST + 1024 + DEFAULT_AIOHTTP_CLIENT_MAX_SIZE
 
-    config = karapace_container.config().set_config_defaults(
-        {
-            "karapace_rest": True,
-            "producer_max_request_size": DEFAULT_PRODUCER_MAX_REQUEST + 1024,
-            "http_request_max_size": 1024,
-        }
-    )
-    assert config.http_request_max_size == 1024
+    config = Config()
+    config.karapace_rest = True
+    config.http_request_max_size = 1024
+    config.producer_max_request_size = DEFAULT_PRODUCER_MAX_REQUEST + 1024
+    assert config.get_max_request_size() == 1024
