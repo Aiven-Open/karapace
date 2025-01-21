618b21f06f67dc5de2336ca5d588d55b132c95fe
Emmanuel Evbuomwan <emmanuel.evbuomwan@aiven.io>
2025-01-21 17:59:21 +0100

    metrics: scope http requests metrics

--- src/schema_registry/telemetry/metrics.py
+++ src/schema_registry/telemetry/metrics.py
@@ -3,25 +3,57 @@ Copyright (c) 2024 Aiven Ltd
 See LICENSE for details
 """
 
+from fastapi import Request, Response, HTTPException
 from opentelemetry.metrics import Counter, Histogram, UpDownCounter
 from schema_registry.telemetry.meter import Meter
 from typing import Final
+from collections.abc import Mapping
+
+import time
 
 
-class Metrics:
+class HTTPRequestMetrics:
     START_TIME_KEY: Final = "start_time"
 
     def __init__(self, meter: Meter):
-        self.karapace_http_requests_in_progress: UpDownCounter = meter.get_meter().create_up_down_counter(
+        self.meter = meter
+        self.karapace_http_requests_in_progress: Final[UpDownCounter] = self.meter.get_meter().create_up_down_counter(
             name="karapace_http_requests_in_progress",
             description="In-progress requests for HTTP/TCP Protocol",
         )
-        self.karapace_http_requests_duration_seconds: Histogram = meter.get_meter().create_histogram(
+        self.karapace_http_requests_duration_seconds: Final[Histogram] = self.meter.get_meter().create_histogram(
             unit="seconds",
             name="karapace_http_requests_duration_seconds",
             description="Request Duration for HTTP/TCP Protocol",
         )
-        self.karapace_http_requests_total: Counter = meter.get_meter().create_counter(
+        self.karapace_http_requests_total: Final[Counter] = self.meter.get_meter().create_counter(
             name="karapace_http_requests_total",
             description="Total Request Count for HTTP/TCP Protocol",
         )
+
+    def get_resource_from_request(self, request: Request) -> str:
+        return request.url.path.split("/")[1]
+
+    def start_request(self, request: Request) -> Mapping[str, str | int]:
+        # Set start time for request
+        setattr(request.state, self.START_TIME_KEY, time.monotonic())
+
+        PATH = request.url.path
+        METHOD = request.method
+        ATTRIBUTES = {"method": METHOD, "path": PATH, "resource": self.get_resource_from_request(request=request)}
+
+        self.karapace_http_requests_in_progress.add(amount=1, attributes=ATTRIBUTES)
+        return ATTRIBUTES
+
+    def finish_request(self, ATTRIBUTES: Mapping[str, str | int], request: Request, response: Response | None) -> None:
+        status = response.status_code if response else 0
+        self.karapace_http_requests_total.add(amount=1, attributes={**ATTRIBUTES, "status": status})
+        self.karapace_http_requests_duration_seconds.record(
+            amount=(time.monotonic() - getattr(request.state, self.START_TIME_KEY)),
+            attributes=ATTRIBUTES,
+        )
+        self.karapace_http_requests_in_progress.add(amount=-1, attributes=ATTRIBUTES)
+
+    def record_request_exception(self, ATTRIBUTES: Mapping[str, str | int], exc: Exception) -> None:
+        status = exc.status_code if isinstance(exc, HTTPException) else 0
+        self.karapace_http_requests_total.add(amount=1, attributes={**ATTRIBUTES, "status": status})
