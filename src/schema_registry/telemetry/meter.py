d10269d24002d826e9a4c0cc386d4885cf079554
Emmanuel Evbuomwan <emmanuel.evbuomwan@aiven.io>
2025-01-22 14:59:35 +0100

    metrics: added `noop` exporter

--- src/schema_registry/telemetry/meter.py
+++ src/schema_registry/telemetry/meter.py
@@ -4,7 +4,7 @@ See LICENSE for details
 """
 
 from dependency_injector.wiring import inject, Provide
-from karapace.config import Config
+from karapace.config import Config, KarapaceTelemetryOTelExporter
 from karapace.container import KarapaceContainer
 from opentelemetry import metrics
 from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
@@ -13,7 +13,32 @@ from opentelemetry.sdk.metrics.export import (
     MetricExporter,
     MetricReader,
     PeriodicExportingMetricReader,
+    MetricsData,
+    MetricExportResult,
 )
+from typing import Any
+
+
+class NOOPMetricExporter(MetricExporter):
+    """Implementation of :class:`MetricExporter` that does nothing.
+
+    This class is intended to be used when metrics exporting to an OTel backend is disabled
+    and the ConsoleExporter is too verbose to be used.
+    """
+
+    def export(
+        self,
+        metrics_data: MetricsData,
+        timeout_millis: float = 0,
+        **kwargs: Any,
+    ) -> MetricExportResult:
+        return MetricExportResult.SUCCESS
+
+    def shutdown(self, timeout_millis: float = 0, **kwargs: Any) -> None:
+        pass
+
+    def force_flush(self, _: float = 0) -> bool:
+        return True
 
 
 class Meter:
@@ -23,11 +48,19 @@ class Meter:
         return metrics.get_meter_provider().get_meter(f"{config.tags.app}.meter")
 
     @staticmethod
+    def get_metric_exporter(config: Config) -> MetricExporter:
+        match config.telemetry.otel_exporter:
+            case KarapaceTelemetryOTelExporter.NOOP:
+                return NOOPMetricExporter()
+            case KarapaceTelemetryOTelExporter.CONSOLE:
+                return ConsoleMetricExporter()
+            case KarapaceTelemetryOTelExporter.OTLP:
+                return OTLPMetricExporter(endpoint=config.telemetry.otel_endpoint_url)
+
+    @staticmethod
     @inject
     def get_metric_reader(config: Config = Provide[KarapaceContainer.config]) -> MetricReader:
-        exporter: MetricExporter = ConsoleMetricExporter()
-        if config.telemetry.otel_endpoint_url:
-            exporter = OTLPMetricExporter(endpoint=config.telemetry.otel_endpoint_url)
+        exporter: MetricExporter = Meter.get_metric_exporter(config)
         return PeriodicExportingMetricReader(
             exporter=exporter,
             export_interval_millis=config.telemetry.metrics_export_interval_milliseconds,
