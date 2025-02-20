129c188f7dd8c9d2ea8bd415eb6eb3fa9849fe0a
Emmanuel Evbuomwan <emmanuel.evbuomwan@aiven.io>
2025-02-20 14:39:35 +0100

    forward_client: added mTLS support
    - fix the server certificates and the associated fixtures
    - add a registry pair started with mTLS

--- src/karapace/core/container.py
+++ src/karapace/core/container.py
@@ -11,5 +11,5 @@ from karapace.core.instrumentation.prometheus import PrometheusInstrumentation
 
 class KarapaceContainer(containers.DeclarativeContainer):
     config = providers.Singleton(Config)
-    forward_client = providers.Singleton(ForwardClient)
+    forward_client = providers.Singleton(ForwardClient, config=config())
     prometheus = providers.Singleton(PrometheusInstrumentation)
