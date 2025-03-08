3a9ba40b5f6565e190e24d27ef6dec454858d3f2
Muralidhar Basani <muralidhar.basani@aiven.io>
2025-03-08 02:37:51 +0100

    Add tests

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -5,13 +5,8 @@ Copyright (c) 2024 Aiven Ltd
 See LICENSE for details
 """
 
-import logging
 import runpy
 from unittest.mock import patch, MagicMock
-
-from _pytest.logging import LogCaptureFixture
-
-from karapace.core.container import KarapaceContainer
 
 from src.karapace import __main__
 
@@ -94,19 +89,16 @@ def test_main_container_initialization():
         assert mock_create_app.call_count == 1
 
 
-def test_uvicorn_run(caplog: LogCaptureFixture, karapace_container: KarapaceContainer) -> None:
-    with caplog.at_level(logging.INFO, logger="karapace.api.factory"):
-        with (
-            patch("karapace.api.factory.create_karapace_application") as mock_create_app,
-            patch("uvicorn.run") as mock_uvicorn_run,
-        ):
-            mock_app = MagicMock()
-            mock_create_app.return_value = mock_app
+def test_uvicorn_run() -> None:
+    with (
+        patch("karapace.api.factory.create_karapace_application") as mock_create_app,
+        patch("uvicorn.run") as mock_uvicorn_run,
+    ):
+        mock_app = MagicMock()
+        mock_create_app.return_value = mock_app
 
-            with patch("sys.argv", ["src.karapace.__main__.py"]):
-                runpy.run_path("src/karapace/__main__.py", run_name="__main__")
+        with patch("sys.argv", ["src.karapace.__main__.py"]):
+            runpy.run_path("src/karapace/__main__.py", run_name="__main__")
 
-            # Verify uvicorn is started with the expected arguments
-            mock_uvicorn_run.assert_called_once_with(
-                mock_app, host="127.0.0.1", port=8081, log_level="debug", log_config=None
-            )
+        # Verify uvicorn is started with the expected arguments
+        mock_uvicorn_run.assert_called_once_with(mock_app, host="127.0.0.1", port=8081, log_level="debug", log_config=None)
