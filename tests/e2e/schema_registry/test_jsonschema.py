68ebe92128a4778961a43d8fd43b7bfa6121db0c
Muralidhar Basani <muralidhar.basani@aiven.io>
2025-07-31 15:55:10 +0200

    Add e2e tests oidc

--- tests/e2e/schema_registry/test_jsonschema.py
+++ tests/e2e/schema_registry/test_jsonschema.py
@@ -1180,3 +1180,34 @@ async def test_schemaregistry_ref(registry_async_client: Client):
         writer=ARRAY_OF_POSITIVE_INTEGER,
         client=registry_async_client,
     )
+
+
+async def test_schema_registry_oidc(
+    registry_async_client_oidc: Client,
+) -> None:
+    subject = new_random_name("subject")
+
+    # sanity check.
+    subject_res = await registry_async_client_oidc.get(f"subjects/{subject}/versions")
+    assert subject_res.status_code == 404, "random subject should no exist {subject}"
+
+    subject_res = await registry_async_client_oidc.post(
+        f"subjects/{subject}/versions",
+        json={
+            "schema": json.dumps({"type": "object", "properties": {"name": {"type": "string"}}, "required": ["name"]}),
+            "schemaType": SchemaType.JSONSCHEMA.value,
+        },
+    )
+    assert subject_res.status_code == 200
+
+
+async def test_schema_registry_oidc_invalid_token(
+    registry_async_client_oidc_invalid: Client,
+) -> None:
+    subject = new_random_name("subject")
+
+    subject_res = await registry_async_client_oidc_invalid.get(f"subjects/{subject}/versions")
+
+    assert subject_res.status_code == 401
+    assert subject_res.json_result["error"] == "Unauthorized"
+    assert subject_res.json_result["reason"] == "Invalid token/payload"
