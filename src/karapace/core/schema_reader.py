f70955eb17e355f8c44772bbc24e9eb0fe70519f
rwaweber <rwaweber@chump.info>
2026-03-04 21:50:47 -0500

    List *all* topics rather than just the config topic
    - Not a big deal since we're just doing a containment operation
    - Turns out I missed this warning on the list_topics operation:
    
    > Warning: If auto.create.topics.enable is set to true on the broker
    > and an unknown topic is specified, it will be created.
    
    [cite](https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html#confluent_kafka.admin.AdminClient.list_topics)

--- src/karapace/core/schema_reader.py
+++ src/karapace/core/schema_reader.py
@@ -231,7 +231,7 @@ class KafkaSchemaReader(Thread, SchemaReaderStoppper):
             # Perform a metadata list, which is allowed in a describe permission, whereas
             # topic-create permissions are a coarser permission grant, not typically given out
             schema_topic_exists = (
-                self.config.topic_name in self.admin_client.list_topics(topic=self.config.topic_name).topics.keys()
+                self.config.topic_name in self.admin_client.list_topics().topics.keys()
             )
             while not self._stop_schema_reader.is_set() and not schema_topic_exists:
                 try:
@@ -341,7 +341,7 @@ class KafkaSchemaReader(Thread, SchemaReaderStoppper):
         except KafkaTimeoutError:
             LOG.warning("Reading begin offsets timed out.")
         except UnknownTopicOrPartitionError:
-            LOG.warning("Topic does not yet exist.")
+            LOG.warning("Topic: %r does not yet exist.", self.config.topic_name)
         except (LeaderNotAvailableError, NotLeaderForPartitionError):
             LOG.warning("Retrying to find leader for schema topic partition.")
         except Exception as e:
